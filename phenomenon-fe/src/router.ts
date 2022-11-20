cea01cab927cbf052040aac8b13a0b706cef86de
aumetra <aumetra@cryptolab.net>
2022-11-20 15:39:47 +0100

    add main and about views

--- phenomenon-fe/src/router.ts
+++ phenomenon-fe/src/router.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [];
+const routes = [
+  { path: '/', component: () => import('./views/MainPage.vue') },
+  { path: '/about', component: () => import('./views/AboutPage.vue') },
+];
 
 export const router = createRouter({
   history: createWebHashHistory(),
--- phenomenon-fe/src/views/MainPage.vue
+++ phenomenon-fe/src/views/MainPage.vue
@@ -1,6 +1,4 @@
-<template>
-  <router-view></router-view>
-</template>
+<template></template>
 
 <script setup lang="ts"></script>
 
