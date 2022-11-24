852342d2534dffac92580bc31909f19a4d8b9d38
aumetra <aumetra@cryptolab.net>
2022-11-24 23:05:36 +0100

    move page location

--- phenomenon-fe/src/router.ts
+++ phenomenon-fe/src/router.ts
@@ -2,12 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   { path: '/', component: () => import('./views/MainPage.vue') },
+  { path: '/about', component: () => import('./views/AboutPage.vue') },
   { path: '/home', component: () => import('./views/MainPage.vue') },
   { path: '/notifications', component: () => import('./views/MainPage.vue') },
   { path: '/messages', component: () => import('./views/MainPage.vue') },
   { path: '/local', component: () => import('./views/MainPage.vue') },
   { path: '/federated', component: () => import('./views/MainPage.vue') },
-  { path: '/about', component: () => import('./views/AboutPage.vue') },
 ];
 
 export const router = createRouter({
