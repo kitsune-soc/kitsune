b541159ac98ae49e19a848ef5bb97c4a03432b61
aumetra <aumetra@cryptolab.net>
2022-11-27 00:05:13 +0100

    clean html from display name and note

--- src/http/graphql/mutation/user.rs
+++ src/http/graphql/mutation/user.rs
@@ -1,5 +1,5 @@
 use super::handle_upload;
-use crate::{db::entity::user, http::graphql::ContextExt};
+use crate::{db::entity::user, http::graphql::ContextExt, util::CleanHtmlExt};
 use async_graphql::{Context, Error, Object, Result, Upload};
 use sea_orm::{ActiveModelTrait, ActiveValue};
 
@@ -19,22 +19,24 @@ impl UserMutation {
         let state = ctx.state();
         let user = ctx.user()?;
         let mut active_user = user::ActiveModel {
-            id: ActiveValue::Unchanged(user.id),
+            id: ActiveValue::Set(user.id),
             ..Default::default()
         };
 
-        if let Some(display_name) = display_name {
+        if let Some(mut display_name) = display_name {
             active_user.display_name = if display_name.is_empty() {
                 ActiveValue::Set(None)
             } else {
+                display_name.clean_html();
                 ActiveValue::Set(Some(display_name))
             };
         }
 
-        if let Some(note) = note {
+        if let Some(mut note) = note {
             active_user.note = if note.is_empty() {
                 ActiveValue::Set(None)
             } else {
+                note.clean_html();
                 ActiveValue::Set(Some(note))
             };
         }
