4149e894a3085aaf3110e367428cc3b296744530
aumetra <aumetra@cryptolab.net>
2022-12-19 00:36:42 +0100

    add mentions to graphql mutation

--- phenomenon/src/consts.rs
+++ phenomenon/src/consts.rs
@@ -1,10 +1,10 @@
+use fancy_regex::Regex;
 use once_cell::sync::Lazy;
-use regex::Regex;
 
 /// - Capture group 1 -> Username
 /// - Caputre group 2 -> Domain
 pub static MENTION_REGEX: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r"(?:^|\W)@([\w\.]+)(?:@(.+\.[[:alnum:]]+))?")
+    Regex::new(r"(?<=\W|^)@([\w\.]+)(?:@([\pL\.\-]+[\pL]+))")
         .expect("Failed to compile mention regex")
 });
 pub const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
--- phenomenon/src/http/graphql/mutation/post.rs
+++ phenomenon/src/http/graphql/mutation/post.rs
@@ -1,13 +1,54 @@
 use crate::{
-    db::model::post::{self, Visibility},
+    consts::MENTION_REGEX,
+    db::model::{
+        account, mention,
+        post::{self, Visibility},
+    },
+    error::Error as ServerError,
     http::graphql::ContextExt,
     sanitize::CleanHtmlExt,
+    state::Zustand,
 };
 use async_graphql::{Context, Error, Object, Result};
 use chrono::Utc;
+use fancy_regex::{Captures, Match};
+use futures_util::FutureExt;
 use pulldown_cmark::{html, Options, Parser};
-use sea_orm::{ActiveModelTrait, EntityTrait, IntoActiveModel, ModelTrait, QueryFilter};
+use sea_orm::{
+    ActiveModelTrait, ColumnTrait, EntityTrait, IntoActiveModel, ModelTrait, QueryFilter,
+    TransactionTrait,
+};
+use tokio::runtime::Handle;
 use uuid::Uuid;
+
+async fn fetch_account(
+    state: &Zustand,
+    username: &str,
+    domain: Option<&str>,
+) -> Result<Option<account::Model>> {
+    if let Some(domain) = domain {
+        let Some(actor_url) = state.webfinger.fetch_actor_url(username, domain).await? else {
+            return Ok(None)
+        };
+
+        state
+            .fetcher
+            .fetch_actor(&actor_url)
+            .await
+            .map(Some)
+            .map_err(Error::from)
+    } else {
+        account::Entity::find()
+            .filter(
+                account::Column::Username
+                    .eq(username)
+                    .and(account::Column::Domain.is_null()),
+            )
+            .one(&state.db_conn)
+            .await
+            .map_err(Error::from)
+    }
+}
 
 #[derive(Default)]
 pub struct PostMutation;
@@ -31,22 +72,69 @@ impl PostMutation {
             buf
         };
 
+        let handle = Handle::current();
+        let (mentioned_account_ids, content) = {
+            let state = state.clone();
+
+            tokio::task::spawn_blocking(move || {
+                let mut mentioned_account_ids = Vec::new();
+                let content = MENTION_REGEX
+                    .replace_all(&content, |capture: &Captures<'_>| {
+                        let username = &capture[1];
+                        let domain = capture.get(2).as_ref().map(Match::as_str);
+                        let Some(account) = handle.block_on(fetch_account(&state, username, domain)).ok().flatten() else {
+                            return capture[0].to_string();
+                        };
+
+                        mentioned_account_ids.push(account.id);
+                        format!("<a href=\"{}\">{}</a>", account.url, &capture[0])
+                    })
+                    .to_string();
+
+                (mentioned_account_ids, content)
+            })
+            .await?
+        };
+
         let id = Uuid::new_v4();
+        let account_id = user.account.id;
         let url = format!("https://{}/posts/{id}", state.config.domain);
-        Ok(post::Model {
-            id,
-            account_id: user.account.id,
-            subject: None,
-            content,
-            is_sensitive,
-            visibility,
-            url,
-            created_at: Utc::now(),
-            updated_at: Utc::now(),
-        }
-        .into_active_model()
-        .insert(&state.db_conn)
-        .await?)
+
+        state
+            .db_conn
+            .transaction(move |tx| {
+                async move {
+                    let post = post::Model {
+                        id,
+                        account_id,
+                        subject: None,
+                        content,
+                        is_sensitive,
+                        visibility,
+                        url,
+                        created_at: Utc::now(),
+                        updated_at: Utc::now(),
+                    }
+                    .into_active_model()
+                    .insert(tx)
+                    .await?;
+
+                    for account_id in mentioned_account_ids {
+                        mention::Model {
+                            account_id,
+                            post_id: post.id,
+                        }
+                        .into_active_model()
+                        .insert(tx)
+                        .await?;
+                    }
+
+                    Ok::<_, ServerError>(post)
+                }
+                .boxed()
+            })
+            .await
+            .map_err(Error::from)
     }
 
     pub async fn delete_post(&self, ctx: &Context<'_>, id: Uuid) -> Result<Uuid> {
@@ -58,6 +146,8 @@ impl PostMutation {
             .one(&state.db_conn)
             .await?
             .ok_or_else(|| Error::new("Post not found"))?;
+
+        // TODO: Send out delete activity
 
         post.delete(&state.db_conn).await?;
 
