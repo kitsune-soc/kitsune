df0431b3ee3ef60b174a030289d831c64714b033
aumetra <aumetra@cryptolab.net>
2022-12-22 16:57:06 +0100

    add pagable outbox

--- phenomenon/src/http/handler/users/outbox.rs
+++ phenomenon/src/http/handler/users/outbox.rs
@@ -8,43 +8,102 @@ use crate::{
     state::Zustand,
 };
 use axum::{
-    extract::{Path, State},
+    extract::{Path, Query, State},
+    response::{IntoResponse, Response},
     Json,
 };
 use phenomenon_type::ap::{
     ap_context,
-    collection::{Collection, CollectionType},
+    collection::{Collection, CollectionPage, CollectionType, PageType},
 };
-use sea_orm::{ColumnTrait, ModelTrait, PaginatorTrait, QueryFilter, Related};
+use sea_orm::{
+    ColumnTrait, ModelTrait, PaginatorTrait, QueryFilter, QueryOrder, QuerySelect, Related,
+};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+const ACTIVITIES_PER_PAGE: u64 = 10;
+
+#[derive(Deserialize, Serialize)]
+pub struct OutboxQuery {
+    max_id: Option<Uuid>,
+    min_id: Option<Uuid>,
+    #[serde(default)]
+    page: bool,
+}
 
 pub async fn get(
     State(state): State<Zustand>,
     Path(username): Path<String>,
-) -> Result<Json<Collection>> {
+    Query(query): Query<OutboxQuery>,
+) -> Result<Response> {
     let Some(account) = <user::Entity as Related<account::Entity>>::find_related()
-        .filter(user::Column::Username.eq(username))
+        .filter(user::Column::Username.eq(username.as_str()))
         .one(&state.db_conn)
         .await?
     else {
         return Err(Error::UserNotFound);
     };
 
-    let public_post_count = account
+    let base_url = format!("https://{}/users/{}/outbox", state.config.domain, username);
+    let base_query = account
         .find_related(post::Entity)
-        .filter(post::Column::Visibility.is_in([Visibility::Public, Visibility::Unlisted]))
-        .count(&state.db_conn)
-        .await?;
+        .filter(post::Column::Visibility.is_in([Visibility::Public, Visibility::Unlisted]));
 
-    let id = format!(
-        "https://{}/users/{}/outbox",
-        state.config.domain, account.username
-    );
-    Ok(Json(Collection {
-        context: ap_context(),
-        id,
-        r#type: CollectionType::OrderedCollection,
-        total_items: public_post_count,
-        first: None,
-        last: None,
-    }))
+    if query.page {
+        let mut id = format!("{base_url}?page=true");
+        let mut page_query = base_query;
+        if let Some(max_id) = query.max_id {
+            id.push_str("&max_id=");
+            id.push_str(&max_id.to_string());
+
+            page_query = page_query.filter(post::Column::Id.lt(max_id));
+        }
+        if let Some(min_id) = query.min_id {
+            id.push_str("&min_id=");
+            id.push_str(&min_id.to_string());
+
+            page_query = page_query.filter(post::Column::Id.gt(min_id));
+        }
+
+        let posts = page_query
+            .limit(ACTIVITIES_PER_PAGE)
+            .order_by_desc(post::Column::Id)
+            .all(&state.db_conn)
+            .await?;
+
+        let prev = format!(
+            "{base_url}?page=true&min_id={}",
+            posts.first().map_or(Uuid::max(), |post| post.id)
+        );
+        let next = format!(
+            "{base_url}?page=true&max_id={}",
+            posts.last().map_or(Uuid::nil(), |post| post.id)
+        );
+
+        Ok(Json(CollectionPage {
+            context: ap_context(),
+            r#type: PageType::OrderedCollectionPage,
+            id,
+            prev,
+            next,
+            part_of: base_url,
+            ordered_items: Vec::new(),
+        })
+        .into_response())
+    } else {
+        let public_post_count = base_query.count(&state.db_conn).await?;
+        let first = format!("{base_url}?page=true");
+        let last = format!("{base_url}?page=true&min_id={}", Uuid::nil());
+
+        Ok(Json(Collection {
+            context: ap_context(),
+            id: base_url,
+            r#type: CollectionType::OrderedCollection,
+            total_items: public_post_count,
+            first: Some(first),
+            last: Some(last),
+        })
+        .into_response())
+    }
 }
