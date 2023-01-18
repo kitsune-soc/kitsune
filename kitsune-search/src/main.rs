7b032c36f8912eecb19cb0371a55f42e606913b0
aumetra <aumetra@cryptolab.net>
2023-01-18 11:27:20 +0100

    set up metrics export for search service

--- kitsune-search/src/main.rs
+++ kitsune-search/src/main.rs
@@ -10,13 +10,31 @@
 extern crate tracing;
 
 use kitsune_search::{config::Configuration, search::SearchIndex};
+use metrics_exporter_prometheus::PrometheusBuilder;
+use metrics_tracing_context::{MetricsLayer, TracingContextLayer};
+use metrics_util::layers::Layer;
+use tracing_subscriber::{layer::SubscriberExt, Registry};
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
-    tracing_subscriber::fmt::init();
-
     let config: Configuration = envy::from_env().unwrap();
+
+    let (prometheus_recorder, server_future) = PrometheusBuilder::new()
+        .with_http_listener(([0, 0, 0, 0], config.prometheus_port))
+        .build()
+        .unwrap();
+    tokio::spawn(server_future);
+
+    let subscriber = Registry::default()
+        .with(tracing_subscriber::fmt::layer())
+        .with(MetricsLayer::new());
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to install tracing subscriber");
+
+    let recorder = TracingContextLayer::all().layer(prometheus_recorder);
+    metrics::set_boxed_recorder(Box::new(recorder)).expect("failed to install metrics recorder");
+
     info!(port = config.port, "Starting up Kitsune search");
 
     let index = SearchIndex::prepare(&config).unwrap();
