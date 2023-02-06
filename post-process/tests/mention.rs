2abf87aac19c7711a4b6cef82f832c2cc79df7ed
aumetra <aumetra@cryptolab.net>
2023-02-06 22:05:35 +0100

    Snapshot testing (#64)
    * add some basic snapshot tests
    
    * some more test -> snapshot
    
    * finish post-process test port

--- post-process/tests/mention.rs
+++ post-process/tests/mention.rs
@@ -1,73 +1,27 @@
 use pest::Parser;
 use post_process::{PostParser, Rule};
-use pretty_assertions::assert_eq;
+use std::fs;
 
 #[test]
 fn invalid_mention() {
-    let text = "@test@hello.world@tes";
-    assert!(PostParser::parse(Rule::mention, text).is_err());
-
-    let text = "@ test@hello.world";
-    assert!(PostParser::parse(Rule::mention, text).is_err());
-
-    let text = "@test@hello.world@tes hello";
-    let mut token_iter = PostParser::parse(Rule::post, text).unwrap();
-    let text1 = token_iter.next().unwrap();
-    assert_eq!(text1.as_rule(), Rule::text);
-    assert_eq!(text1.as_str(), "@test@hello.world@tes hello");
-    assert!(token_iter.next().is_none());
-
-    let text = "@\\test@hello.world";
-    assert!(PostParser::parse(Rule::mention, text).is_err());
-
-    let text = "@test@hel\\lo.world";
-    assert!(PostParser::parse(Rule::mention, text).is_err());
+    insta::glob!("input/mention/invalid_*", |path| {
+        let mention = fs::read_to_string(path).unwrap();
+        insta::assert_debug_snapshot!(PostParser::parse(Rule::post, &mention).unwrap());
+    });
 }
 
 #[test]
 fn subdomain_mention() {
-    let text = "@test@social.hello.world";
-    let mut token_iter = PostParser::parse(Rule::mention, text).unwrap();
-    let mention = token_iter.next().unwrap();
-    assert_eq!(mention.as_str(), "@test@social.hello.world");
+    insta::glob!("input/mention/subdomain_*", |path| {
+        let mention = fs::read_to_string(path).unwrap();
+        insta::assert_debug_snapshot!(PostParser::parse(Rule::mention, &mention).unwrap());
+    })
 }
 
 #[test]
 fn parse_mention() {
-    let text = "hello @桐生@friday.night @真島! ";
-    let mut token_iter = PostParser::parse(Rule::post, text).expect("Failed to parse post");
-
-    let text1 = token_iter.next().unwrap();
-    assert_eq!(text1.as_rule(), Rule::text);
-    assert_eq!(text1.as_str(), "hello");
-
-    let mention1 = token_iter.next().unwrap();
-    assert_eq!(mention1.as_rule(), Rule::mention);
-    assert_eq!(mention1.as_str(), " @桐生@friday.night");
-
-    let mut mention1 = mention1.into_inner();
-    let mention1_prefix = mention1.next().unwrap();
-    assert_eq!(mention1_prefix.as_rule(), Rule::component_prefix);
-    assert_eq!(mention1_prefix.as_str(), " ");
-    let mention1_username = mention1.next().unwrap();
-    assert_eq!(mention1_username.as_rule(), Rule::mention_username);
-    assert_eq!(mention1_username.as_str(), "桐生");
-    let mention1_domain = mention1.next().unwrap();
-    assert_eq!(mention1_domain.as_rule(), Rule::mention_domain);
-    assert_eq!(mention1_domain.as_str(), "friday.night");
-
-    let mention2 = token_iter.next().unwrap();
-    assert_eq!(mention2.as_rule(), Rule::mention);
-    assert_eq!(mention2.as_str(), " @真島");
-    let mut mention2 = mention2.into_inner();
-    let mention2_prefix = mention2.next().unwrap();
-    assert_eq!(mention2_prefix.as_rule(), Rule::component_prefix);
-    assert_eq!(mention2_prefix.as_str(), " ");
-    let mention2_username = mention2.next().unwrap();
-    assert_eq!(mention2_username.as_rule(), Rule::mention_username);
-    assert_eq!(mention2_username.as_str(), "真島");
-
-    let text2 = token_iter.next().unwrap();
-    assert_eq!(text2.as_rule(), Rule::text);
-    assert_eq!(text2.as_str(), "! ");
+    insta::glob!("input/mention/full_post_*", |path| {
+        let post = fs::read_to_string(path).unwrap();
+        insta::assert_debug_snapshot!(PostParser::parse(Rule::post, &post).unwrap());
+    });
 }
--- post-process/tests/mixed.rs
+++ post-process/tests/mixed.rs
@@ -3,8 +3,8 @@ use post_process::{PostParser, Rule};
 use std::fs;
 
 #[test]
-fn parse_emote() {
-    insta::glob!("input/emote/full_post_*", |path| {
+fn mixed_content() {
+    insta::glob!("input/mixed/mixed_content_*", |path| {
         let post = fs::read_to_string(path).unwrap();
         insta::assert_debug_snapshot!(PostParser::parse(Rule::post, &post).unwrap());
     });
