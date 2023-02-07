e8c0977c817118cb51bd5b93930ebb1e5cbb6acb
aumetra <aumetra@cryptolab.net>
2023-02-07 13:17:44 +0100

    add logic

--- kitsune/src/http/handler/nodeinfo/two_one.rs
+++ kitsune/src/http/handler/nodeinfo/two_one.rs
@@ -1,10 +1,42 @@
-use crate::state::Zustand;
+use crate::{error::Result, state::Zustand};
 use axum::{debug_handler, extract::State, routing, Json, Router};
-use kitsune_type::nodeinfo::two_one::TwoOne;
+use kitsune_db::entity::prelude::{Posts, Users};
+use kitsune_type::nodeinfo::two_one::{
+    Protocol, Services, Software, TwoOne, Usage, UsageUsers, Version,
+};
+use sea_orm::{EntityTrait, PaginatorTrait};
+use serde_json::Value;
 
 #[debug_handler]
-async fn get(State(state): State<Zustand>) -> Json<TwoOne> {
-    todo!();
+async fn get(State(state): State<Zustand>) -> Result<Json<TwoOne>> {
+    let total = Users::find().count(&state.db_conn).await?;
+    let local_posts = Posts::find().count(&state.db_conn).await?; // TODO: actually filter out local posts
+
+    Ok(Json(TwoOne {
+        version: Version::TwoOne,
+        software: Software {
+            name: env!("CARGO_PKG_NAME").into(),
+            version: env!("CARGO_PKG_VERSION").into(),
+            repository: env!("CARGO_PKG_REPOSITORY").into(),
+            homepage: Some(env!("CARGO_PKG_HOMEPAGE").into()),
+        },
+        protocols: vec![Protocol::ActivityPub],
+        services: Services {
+            inbound: vec![],
+            outbound: vec![],
+        },
+        open_registrations: true,
+        usage: Usage {
+            users: UsageUsers {
+                total,
+                active_halfyear: None,
+                active_month: None,
+            },
+            local_comments: None,
+            local_posts,
+        },
+        metadata: Value::Null,
+    }))
 }
 
 pub fn routes() -> Router<Zustand> {
