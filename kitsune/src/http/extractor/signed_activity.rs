9deacaf91284b9b5e3e14d3b487e67850423860e
aumetra <aumetra@cryptolab.net>
2023-03-10 11:16:19 +0100

    Split the extractor module (#99)
    * split module
    
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/e5530aba13caff5a4f41713f1265b754dc2abfd8' (2023-02-11)
      → 'github:nixos/nixpkgs/c90c4025bb6e0c4eaf438128a3b2640314b1c58d' (2023-03-08)
    • Updated input 'rust-overlay':
        'github:oxalica/rust-overlay/1bd5d7bb2f31cbc43fb8f722e3a39a45ee4dcec8' (2023-02-13)
      → 'github:oxalica/rust-overlay/9f687941160108f89dd8bd2c650d32c15a35c7c7' (2023-03-10)
    
    * update deps
    
    * conditional reexport

---
+++ kitsune/src/http/extractor/signed_activity.rs
@@ -0,0 +1,87 @@
+use crate::{
+    activitypub::fetcher::FetchOptions,
+    error::{Error, Result},
+    state::Zustand,
+};
+use async_trait::async_trait;
+use axum::{
+    body::Body,
+    extract::FromRequest,
+    response::{IntoResponse, Response},
+    RequestExt,
+};
+use http::{request::Parts, StatusCode};
+use kitsune_db::entity::accounts;
+use kitsune_http_signatures::{
+    ring::signature::{UnparsedPublicKey, RSA_PKCS1_2048_8192_SHA256},
+    HttpVerifier,
+};
+use kitsune_type::ap::Activity;
+use rsa::pkcs8::{Document, SubjectPublicKeyInfo};
+
+pub struct SignedActivity(pub accounts::Model, pub Activity);
+
+#[async_trait]
+impl FromRequest<Zustand, Body> for SignedActivity {
+    type Rejection = Response;
+
+    async fn from_request(
+        req: http::Request<Body>,
+        state: &Zustand,
+    ) -> Result<Self, Self::Rejection> {
+        let (parts, body) = req
+            .with_limited_body()
+            .expect("[Bug] Payload size of inbox not limited")
+            .into_parts();
+
+        let activity: Activity = match hyper::body::to_bytes(body).await {
+            Ok(bytes) => serde_json::from_slice(&bytes).map_err(Error::from)?,
+            Err(err) => {
+                debug!(error = %err, "Failed to buffer body");
+                return Err(StatusCode::INTERNAL_SERVER_ERROR.into_response());
+            }
+        };
+        let ap_id = activity
+            .rest
+            .attributed_to()
+            .ok_or_else(|| StatusCode::BAD_REQUEST.into_response())?;
+
+        let remote_user = state.fetcher.fetch_actor(ap_id.into()).await?;
+        if !verify_signature(&parts, &remote_user).await? {
+            // Refetch the user and try again
+            // Maybe they rekeyed
+
+            let opts = FetchOptions::builder()
+                .refetch(true)
+                .url(ap_id)
+                .build()
+                .unwrap();
+            let remote_user = state.fetcher.fetch_actor(opts).await?;
+
+            if !verify_signature(&parts, &remote_user).await? {
+                return Err(StatusCode::UNAUTHORIZED.into_response());
+            }
+        }
+
+        Ok(Self(remote_user, activity))
+    }
+}
+
+async fn verify_signature(parts: &Parts, remote_user: &accounts::Model) -> Result<bool> {
+    let (_tag, public_key) = Document::from_pem(&remote_user.public_key).map_err(Error::from)?;
+    let public_key: SubjectPublicKeyInfo<'_> = public_key.decode_msg().map_err(Error::from)?;
+    let public_key = UnparsedPublicKey::new(
+        &RSA_PKCS1_2048_8192_SHA256,
+        public_key.subject_public_key.to_vec(),
+    );
+
+    let is_valid = HttpVerifier::default()
+        .verify(parts, |_key_id| async move {
+            // TODO: Select from the database by key ID
+            Ok(public_key)
+        })
+        .await
+        .is_ok();
+
+    Ok(is_valid)
+}
