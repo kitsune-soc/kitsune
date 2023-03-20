b2624d41a202810828036b83c539b9c8e9fb7ead
aumetra <aumetra@cryptolab.net>
2023-03-20 19:00:21 +0100

    move to job dispatcher model

--- kitsune/src/job/mod.rs
+++ kitsune/src/job/mod.rs
@@ -11,6 +11,7 @@ use crate::{
     state::Zustand,
 };
 use chrono::Utc;
+use futures_util::{stream::FuturesUnordered, StreamExt};
 use kitsune_db::entity::jobs;
 use kitsune_db::{custom::JobState, entity::prelude::Jobs};
 use once_cell::sync::Lazy;
@@ -25,8 +26,9 @@ mod catch_panic;
 
 pub mod deliver;
 
+const PAUSE_BETWEEN_QUERIES: Duration = Duration::from_secs(5);
 const MAX_CONCURRENT_REQUESTS: usize = 10;
-const PAUSE_BETWEEN_QUERIES: Duration = Duration::from_secs(10);
+const EXECUTION_TIMEOUT_DURATION: Duration = Duration::from_secs(30);
 static LINEAR_BACKOFF_DURATION: Lazy<chrono::Duration> = Lazy::new(|| chrono::Duration::minutes(1)); // One minute
 
 #[derive(Deserialize, Serialize)]
@@ -37,112 +39,133 @@ pub enum Job {
     DeliverUnfavourite(UnfavouriteDeliveryContext),
 }
 
-async fn get_job(db_conn: &DatabaseConnection) -> Result<Option<jobs::Model>> {
+async fn execute_one(db_job: jobs::Model, state: Zustand, deliverer: Deliverer) {
+    let job: Job = serde_json::from_value(db_job.context.clone())
+        .expect("[Bug] Failed to deserialise job context");
+
+    let execution_result = CatchPanic::new(async {
+        match job {
+            Job::DeliverCreate(ctx) => self::deliver::create::run(&state, &deliverer, ctx).await,
+            Job::DeliverDelete(ctx) => self::deliver::delete::run(&state, &deliverer, ctx).await,
+            Job::DeliverFavourite(ctx) => {
+                self::deliver::favourite::run(&state, &deliverer, ctx).await
+            }
+            Job::DeliverUnfavourite(ctx) => {
+                self::deliver::unfavourite::run(&state, &deliverer, ctx).await
+            }
+        }
+    })
+    .await;
+
+    if let Ok(Err(ref err)) = execution_result {
+        error!(error = %err, "Job execution failed");
+    }
+
+    let mut update_model = db_job.clone().into_active_model();
+    #[allow(clippy::cast_possible_truncation)]
+    match execution_result {
+        Ok(Err(..)) | Err(..) => {
+            increment_counter!("failed_jobs");
+
+            update_model.state = ActiveValue::Set(JobState::Failed);
+            update_model.fail_count = ActiveValue::Set(db_job.fail_count + 1);
+            update_model.run_at = ActiveValue::Set(
+                (Utc::now() + (*LINEAR_BACKOFF_DURATION * db_job.fail_count)).into(),
+            );
+            update_model.updated_at = ActiveValue::Set(Utc::now().into());
+        }
+        _ => {
+            increment_counter!("succeeded_jobs");
+
+            update_model.state = ActiveValue::Set(JobState::Succeeded);
+            update_model.updated_at = ActiveValue::Set(Utc::now().into());
+        }
+    }
+
+    if let Err(err) = update_model.update(&state.db_conn).await {
+        error!(error = %err, "Failed to update job information");
+    }
+}
+
+async fn get_jobs(db_conn: &DatabaseConnection, num_jobs: usize) -> Result<Vec<jobs::Model>> {
     let txn = db_conn.begin().await?;
 
-    let Some(job) = Jobs::find()
+    let jobs = Jobs::find()
         .filter(
-            jobs::Column::State.eq(JobState::Queued)
+            jobs::Column::State
+                .eq(JobState::Queued)
                 .or(jobs::Column::State.eq(JobState::Failed))
                 .and(jobs::Column::RunAt.lte(Utc::now()))
                 // Re-execute job if it has been running for longer than an hour (probably the worker crashed or something)
-                .or(
-                    jobs::Column::State.eq(JobState::Running)
-                        .and(jobs::Column::UpdatedAt.lt(Utc::now() - chrono::Duration::hours(1))),
-                ),
+                .or(jobs::Column::State
+                    .eq(JobState::Running)
+                    .and(jobs::Column::UpdatedAt.lt(Utc::now() - chrono::Duration::hours(1)))),
         )
+        .limit(num_jobs as u64)
         .order_by_asc(jobs::Column::CreatedAt)
         .lock_exclusive()
-        .one(&txn)
+        .all(&txn)
         .await
-        .map_err(Error::from)?
-    else {
-        return Ok(None);
-    };
+        .map_err(Error::from)?;
 
-    let mut update_job = job.into_active_model();
-    update_job.state = ActiveValue::Set(JobState::Running);
-    update_job.updated_at = ActiveValue::Set(Utc::now().into());
-    let job = update_job.update(&txn).await?;
+    let update_jobs = jobs.iter().map(|job| {
+        let mut update_job = job.clone().into_active_model();
+        update_job.state = ActiveValue::Set(JobState::Running);
+        update_job.updated_at = ActiveValue::Set(Utc::now().into());
+        update_job
+    });
+
+    for update_job in update_jobs {
+        Jobs::update(update_job).exec(&txn).await?;
+    }
 
     txn.commit().await?;
 
-    Ok(Some(job))
+    Ok(jobs)
 }
 
 #[instrument(skip(state))]
-pub async fn run(state: Zustand) {
-    let mut interval = tokio::time::interval(PAUSE_BETWEEN_QUERIES);
+pub async fn run_dispatcher(state: Zustand, num_job_workers: usize) {
     let deliverer = Deliverer::default();
+    let mut executor = FuturesUnordered::new();
+    let mut execution_timeout = tokio::time::interval(EXECUTION_TIMEOUT_DURATION);
 
-    let mut found_job = false;
+    let mut pause_between_queries = tokio::time::interval(PAUSE_BETWEEN_QUERIES);
+    let mut do_pause = false;
 
     loop {
-        if !found_job {
-            interval.tick().await;
-            found_job = true;
+        if do_pause {
+            pause_between_queries.tick().await;
+            do_pause = false;
         }
 
-        let db_job = match get_job(&state.db_conn).await {
-            Ok(Some(job)) => job,
-            Ok(None) => {
-                found_job = false;
-                continue;
-            }
+        let num_jobs = num_job_workers - executor.len();
+        let jobs = match get_jobs(&state.db_conn, num_jobs).await {
+            Ok(jobs) => jobs,
             Err(err) => {
-                error!(error = %err, "Failed to load job from database");
+                error!(error = %err, "Failed to get jobs from database");
                 continue;
             }
         };
 
-        let job: Job = serde_json::from_value(db_job.context.clone())
-            .expect("[Bug] Failed to deserialise job context");
-
-        let execution_result = CatchPanic::new(async {
-            match job {
-                Job::DeliverCreate(ctx) => {
-                    self::deliver::create::run(&state, &deliverer, ctx).await
-                }
-                Job::DeliverDelete(ctx) => {
-                    self::deliver::delete::run(&state, &deliverer, ctx).await
-                }
-                Job::DeliverFavourite(ctx) => {
-                    self::deliver::favourite::run(&state, &deliverer, ctx).await
-                }
-                Job::DeliverUnfavourite(ctx) => {
-                    self::deliver::unfavourite::run(&state, &deliverer, ctx).await
-                }
-            }
-        })
-        .await;
-
-        if let Ok(Err(ref err)) = execution_result {
-            error!(error = %err, "Job execution failed");
+        if jobs.is_empty() && executor.is_empty() {
+            do_pause = true;
+            continue;
         }
 
-        let mut update_model = db_job.clone().into_active_model();
-        #[allow(clippy::cast_possible_truncation)]
-        match execution_result {
-            Ok(Err(..)) | Err(..) => {
-                increment_counter!("failed_jobs");
-
-                update_model.state = ActiveValue::Set(JobState::Failed);
-                update_model.fail_count = ActiveValue::Set(db_job.fail_count + 1);
-                update_model.run_at = ActiveValue::Set(
-                    (Utc::now() + (*LINEAR_BACKOFF_DURATION * db_job.fail_count)).into(),
-                );
-                update_model.updated_at = ActiveValue::Set(Utc::now().into());
-            }
-            _ => {
-                increment_counter!("succeeded_jobs");
-
-                update_model.state = ActiveValue::Set(JobState::Succeeded);
-                update_model.updated_at = ActiveValue::Set(Utc::now().into());
-            }
+        for job in jobs {
+            executor.push(tokio::spawn(execute_one(
+                job,
+                state.clone(),
+                deliverer.clone(),
+            )));
         }
 
-        if let Err(err) = update_model.update(&state.db_conn).await {
-            error!(error = %err, "Failed to update job information");
+        loop {
+            tokio::select! {
+                None = executor.next() => break,
+                _ = execution_timeout.tick() => break,
+            }
         }
     }
 }
