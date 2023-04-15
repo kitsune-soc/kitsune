e28fc2bc6b7a49258b2cb15c79ab38c7e5bdc174
aumetra <aumetra@cryptolab.net>
2023-04-15 14:13:18 +0200

    move around consts

--- kitsune/src/job/mod.rs
+++ kitsune/src/job/mod.rs
@@ -30,9 +30,9 @@ mod catch_panic;
 
 pub mod deliver;
 
-const PAUSE_BETWEEN_QUERIES: Duration = Duration::from_secs(5);
-const MAX_CONCURRENT_REQUESTS: usize = 10;
 const EXECUTION_TIMEOUT_DURATION: Duration = Duration::from_secs(30);
+const MAX_CONCURRENT_REQUESTS: usize = 10;
+const PAUSE_BETWEEN_QUERIES: Duration = Duration::from_secs(5);
 
 #[enum_dispatch(Runnable)]
 #[derive(Deserialize, Serialize)]
