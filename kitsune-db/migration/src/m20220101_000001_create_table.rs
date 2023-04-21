f6b44eb899575b7ae0a829bfdd6a18abae271367
aumetra <aumetra@cryptolab.net>
2023-04-21 19:35:41 +0200

    remove the username unique requirement

--- kitsune-db/migration/src/m20220101_000001_create_table.rs
+++ kitsune-db/migration/src/m20220101_000001_create_table.rs
@@ -129,12 +129,7 @@ impl MigrationTrait for Migration {
                             .unique_key(),
                     )
                     .col(ColumnDef::new(Users::OidcId).text().unique_key())
-                    .col(
-                        ColumnDef::new(Users::Username)
-                            .text()
-                            .not_null()
-                            .unique_key(),
-                    )
+                    .col(ColumnDef::new(Users::Username).text().not_null())
                     .col(ColumnDef::new(Users::Email).text().not_null().unique_key())
                     .col(ColumnDef::new(Users::Password).text().unique_key())
                     .col(ColumnDef::new(Users::Domain).text().not_null())
