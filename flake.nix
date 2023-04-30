5e20fec655fd75ecd99216386c432fcd8e7646e4
aumetra <aumetra@cryptolab.net>
2023-04-30 19:42:01 +0200

    deduplicate

--- flake.nix
+++ flake.nix
@@ -1,88 +1,63 @@
 {
-    inputs = {
-        flake-utils.url = "github:numtide/flake-utils";
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        rust-overlay.url = "github:oxalica/rust-overlay";
-    };
-    outputs = { self, flake-utils, nixpkgs, rust-overlay }:
-        flake-utils.lib.eachDefaultSystem (system:
-            let
-                overlays = [ (import rust-overlay) ];
-                pkgs = import nixpkgs {
-                    inherit overlays system;
-                };
-                rustPlatform = pkgs.makeRustPlatform {
-                    cargo = pkgs.rust-bin.stable.latest.default;
-                    rustc = pkgs.rust-bin.stable.latest.default;
-                };
-            in
-            {
-                packages.cli = rustPlatform.buildRustPackage {
-                    pname = "kitsune-cli";
-                    version = "0.0.1-pre.0";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { self, flake-utils, nixpkgs, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit overlays system;
+        };
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = pkgs.rust-bin.stable.latest.default;
+          rustc = pkgs.rust-bin.stable.latest.default;
+        };
+        basePackage = {
+          version = "0.0.1-pre.0";
+          meta = {
+            description = "ActivityPub-federated microblogging";
+            homepage = "https://joinkitsune.org";
+          };
 
-                    meta = {
-                        description = "ActivityPub-federated microblogging";
-                        homepage = "https://joinkitsune.org";
-                    };
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
 
-                    cargoBuildFlags = "-p kitsune-cli";
-                    cargoLock = {
-                        lockFile = ./Cargo.lock;
-                        allowBuiltinFetchGit = true;
-                    };
-
-                    src = ./.;
-                };
-                packages.main = rustPlatform.buildRustPackage {
-                    pname = "kitsune";
-                    version = "0.0.1-pre.0";
-
-                    meta = {
-                        description = "ActivityPub-federated microblogging";
-                        homepage = "https://joinkitsune.org";
-                    };
-
-                    buildFeatures = [ "meilisearch" ];
-                    cargoBuildFlags = "-p kitsune";
-                    cargoLock = {
-                        lockFile = ./Cargo.lock;
-                        allowBuiltinFetchGit = true;
-                    };
-
-                    src = ./.;
-                };
-                packages.search = rustPlatform.buildRustPackage {
-                    pname = "kitsune-search";
-                    version = "0.0.1-pre.0";
-
-                    meta = {
-                        description = "ActivityPub-federated microblogging";
-                        homepage = "https://joinkitsune.org";
-                    };
-
-                    cargoBuildFlags = "-p kitsune-search";
-                    cargoLock = {
-                        lockFile = ./Cargo.lock;
-                        allowBuiltinFetchGit = true;
-                    };
-
-                    src = ./.;
-                };
-                devShells.default = pkgs.mkShell {
-                    buildInputs = with pkgs; [
-                        cargo-insta
-                        dhall
-                        nodejs
-                        openssl
-                        protobuf
-                        redis
-                        rust-bin.stable.latest.default
-                        sqlite
-                        yarn
-                        zlib
-                    ];
-                };
-            }
-        );
+          src = ./.;
+        };
+      in
+      {
+        packages.cli = rustPlatform.buildRustPackage (basePackage // {
+          pname = "kitsune-cli";
+          cargoBuildFlags = "-p kitsune-cli";
+        });
+        packages.main = rustPlatform.buildRustPackage (basePackage // {
+          pname = "kitsune";
+          buildFeatures = [ "meilisearch" ];
+          cargoBuildFlags = "-p kitsune";
+        });
+        packages.search = rustPlatform.buildRustPackage (basePackage // {
+          pname = "kitsune-search";
+          cargoBuildFlags = "-p kitsune-search";
+        });
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo-insta
+            dhall
+            nodejs
+            openssl
+            protobuf
+            redis
+            rust-bin.stable.latest.default
+            sqlite
+            yarn
+            zlib
+          ];
+        };
+      }
+    );
 }
