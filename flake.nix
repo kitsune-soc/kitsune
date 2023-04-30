f0c10ddc1ddb6e14dc947a7bae08f50bfeff349c
aumetra <aumetra@cryptolab.net>
2023-04-30 15:00:53 +0200

    base flake build derivations

--- flake.nix
+++ flake.nix
@@ -11,15 +11,70 @@
                 pkgs = import nixpkgs {
                     inherit overlays system;
                 };
+                rustPlatform = pkgs.makeRustPlatform {
+                    cargo = pkgs.rust-bin.stable.latest.default;
+                    rustc = pkgs.rust-bin.stable.latest.default;
+                };
             in
             {
+                packages.cli = rustPlatform.buildRustPackage {
+                    pname = "kitsune-cli";
+                    version = "0.0.1-pre.0";
+
+                    meta = {
+                        description = "ActivityPub-federated microblogging";
+                        homepage = "https://joinkitsune.org";
+                    };
+
+                    cargoBuildFlags = "-p kitsune-cli";
+                    cargoLock = {
+                        lockFile = ./Cargo.lock;
+                        allowBuiltinFetchGit = true;
+                    };
+
+                    src = ./.;
+                };
+                packages.main = rustPlatform.buildRustPackage {
+                    pname = "kitsune";
+                    version = "0.0.1-pre.0";
+
+                    meta = {
+                        description = "ActivityPub-federated microblogging";
+                        homepage = "https://joinkitsune.org";
+                    };
+
+                    buildFeatures = [ "meilisearch" ];
+                    cargoBuildFlags = "-p kitsune";
+                    cargoLock = {
+                        lockFile = ./Cargo.lock;
+                        allowBuiltinFetchGit = true;
+                    };
+
+                    src = ./.;
+                };
+                packages.search = rustPlatform.buildRustPackage {
+                    pname = "kitsune-search";
+                    version = "0.0.1-pre.0";
+
+                    meta = {
+                        description = "ActivityPub-federated microblogging";
+                        homepage = "https://joinkitsune.org";
+                    };
+
+                    cargoBuildFlags = "-p kitsune-search";
+                    cargoLock = {
+                        lockFile = ./Cargo.lock;
+                        allowBuiltinFetchGit = true;
+                    };
+
+                    src = ./.;
+                };
                 devShells.default = pkgs.mkShell {
                     buildInputs = with pkgs; [
                         cargo-insta
                         dhall
                         nodejs
                         openssl
-                        #postgresql_15
                         protobuf
                         redis
                         rust-bin.stable.latest.default
