b0371abdf6122284288ebeb09e052a0ce143b6b3
aumetra <aumetra@cryptolab.net>
2023-05-26 12:46:19 +0200

    Update dependencies (#220)
    * update cargo deps
    
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/48a0fb7aab511df92a17cf239c37f2bd2ec9ae3a' (2023-05-18)
      → 'github:nixos/nixpkgs/f91ee3065de91a3531329a674a45ddcb3467a650' (2023-05-24)
    • Updated input 'rust-overlay':
        'github:oxalica/rust-overlay/7c9a265c2eaa5783bc18593b1aec39a85653c076' (2023-05-18)
      → 'github:oxalica/rust-overlay/b51083ca078b79db94e954e63655ba754956d7ef' (2023-05-26)
    
    * fix deprecation warnings

--- kitsune-db/src/model/account.rs
+++ kitsune-db/src/model/account.rs
@@ -1,6 +1,6 @@
 use crate::{error::EnumConversionError, schema::accounts};
 use diesel::{
-    backend::RawValue,
+    backend::Backend,
     deserialize::{self, FromSql},
     pg::Pg,
     serialize::{self, Output, ToSql},
@@ -154,7 +154,7 @@ impl FromSql<Integer, Pg> for ActorType
 where
     i32: FromSql<Integer, Pg>,
 {
-    fn from_sql(bytes: RawValue<'_, Pg>) -> deserialize::Result<Self> {
+    fn from_sql(bytes: <Pg as Backend>::RawValue<'_>) -> deserialize::Result<Self> {
         let value = i32::from_sql(bytes)?;
         Ok(Self::from_i32(value).ok_or(EnumConversionError(value))?)
     }
