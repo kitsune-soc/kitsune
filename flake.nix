5e4c41f909950b00b565f777954dbd171b38ec89
Wim Looman <git@nemo157.com>
2023-07-10 18:59:01 +0200

    Use version from workspace Cargo.toml for nix flake

--- flake.nix
+++ flake.nix
@@ -23,9 +23,9 @@
           zlib
         ];
         cargoConfig = builtins.fromTOML (builtins.readFile ./.cargo/config.toml);  # TODO: Set the target CPU conditionally
-        cargoToml = builtins.fromTOML (builtins.readFile ./kitsune/Cargo.toml);
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
         basePackage = {
-          inherit (cargoToml.package) version;
+          inherit (cargoToml.workspace.package) version;
 
           meta = {
             description = "ActivityPub-federated microblogging";
