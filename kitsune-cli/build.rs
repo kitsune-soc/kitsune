d14a87976f6da9d135be14238ab76922494b1524
aumetra <aumetra@cryptolab.net>
2023-07-25 12:10:42 +0200

    Define a fallback value in the build scripts

--- kitsune-cli/build.rs
+++ kitsune-cli/build.rs
@@ -5,5 +5,5 @@ fn main() {
         .all_git()
         .git_sha(true)
         .emit_and_set()
-        .unwrap();
+        .unwrap_or_else(|_| println!("cargo:rustc-env=VERGEN_GIT_SHA=unknown"));
 }
