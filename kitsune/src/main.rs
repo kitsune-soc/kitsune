dd1cce676950da2c661ba34916792e65a415e1a8
aumetra <aumetra@cryptolab.net>
2023-08-02 21:06:58 +0200

    fix compile

--- kitsune/src/main.rs
+++ kitsune/src/main.rs
@@ -68,7 +68,10 @@ fn initialise_metrics<S>(config: &Configuration) -> impl Layer<S>
 where
     S: for<'a> tracing_subscriber::registry::LookupSpan<'a> + tracing::Subscriber,
 {
-    use axum_prometheus::{AXUM_HTTP_REQUESTS_DURATION_SECONDS, SECONDS_DURATION_BUCKETS};
+    use axum_prometheus::{
+        utils::SECONDS_DURATION_BUCKETS, AXUM_HTTP_REQUESTS_DURATION_SECONDS,
+        PREFIXED_HTTP_REQUESTS_DURATION_SECONDS,
+    };
     use metrics_exporter_prometheus::{Matcher, PrometheusBuilder};
     use metrics_tracing_context::{MetricsLayer, TracingContextLayer};
     use metrics_util::layers::Layer as _;
@@ -76,13 +79,19 @@ where
     let (prometheus_recorder, server_future) = PrometheusBuilder::new()
         // Some defaults that would have been set by the `axum-prometheus` crate
         .set_buckets_for_metric(
-            Matcher::Full(AXUM_HTTP_REQUESTS_DURATION_SECONDS.to_string()),
+            Matcher::Full(
+                PREFIXED_HTTP_REQUESTS_DURATION_SECONDS
+                    .get()
+                    .map_or(AXUM_HTTP_REQUESTS_DURATION_SECONDS, |s| s.as_str())
+                    .to_string(),
+            ),
             SECONDS_DURATION_BUCKETS,
         )
         .unwrap()
         .with_http_listener(([0, 0, 0, 0], config.server.prometheus_port))
         .build()
         .unwrap();
+
     tokio::spawn(server_future);
 
     let recorder = TracingContextLayer::all().layer(prometheus_recorder);
