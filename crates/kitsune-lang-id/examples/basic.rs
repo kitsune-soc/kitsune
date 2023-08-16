929cf234e9085c8843899608421679c22f294bcf
aumetra <aumetra@cryptolab.net>
2023-08-16 12:24:38 +0200

    disable whichlang feature by default

--- crates/kitsune-lang-id/examples/basic.rs
+++ crates/kitsune-lang-id/examples/basic.rs
@@ -1,7 +1,6 @@
-use kitsune_lang_id::DetectionBackend;
-
 fn main() {
-    kitsune_lang_id::set_backend(DetectionBackend::Whichlang);
+    #[cfg(feature = "whichlang")]
+    kitsune_lang_id::set_backend(kitsune_lang_id::DetectionBackend::Whichlang);
 
     let detected = kitsune_lang_id::detect_language("das ist schon eine coole library..");
     println!("Detected language: {detected}");
