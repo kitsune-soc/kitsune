852722783123910415bb8d4626b14f46a2a29115
aumetra <aumetra@cryptolab.net>
2023-08-23 19:06:46 +0200

    fix oidc

--- kitsune/src/http/handler/oidc/callback.rs
+++ kitsune/src/http/handler/oidc/callback.rs
@@ -1,5 +1,5 @@
 use crate::{
-    error::{ApiError, Result},
+    error::{ApiError, Error, Result},
     service::{
         oauth2::{AuthorisationCode, OAuth2Service},
         oidc::OidcService,
@@ -25,7 +25,7 @@ pub struct CallbackQuery {
 }
 
 pub async fn get(
-    State(db_conn): State<PgPool>,
+    State(db_pool): State<PgPool>,
     State(user_service): State<UserService>,
     State(oauth_service): State<OAuth2Service>,
     State(oidc_service): State<Option<OidcService>>,
@@ -36,14 +36,18 @@ pub async fn get(
     };
 
     let user_info = oidc_service.get_user_info(query.state, query.code).await?;
+    let user = db_pool
+        .with_connection(|mut db_conn| async move {
+            users::table
+                .filter(users::oidc_id.eq(&user_info.subject))
+                .get_result(&mut db_conn)
+                .await
+                .optional()
+                .map_err(Error::from)
+        })
+        .await?;
 
-    let mut db_conn = db_conn.get().await?;
-    let user = if let Some(user) = users::table
-        .filter(users::oidc_id.eq(&user_info.subject))
-        .get_result(&mut db_conn)
-        .await
-        .optional()?
-    {
+    let user = if let Some(user) = user {
         user
     } else {
         let register = Register::builder()
@@ -55,9 +59,14 @@ pub async fn get(
         user_service.register(register).await?
     };
 
-    let application = oauth2_applications::table
-        .find(user_info.oauth2.application_id)
-        .get_result(&mut db_conn)
+    let application = db_pool
+        .with_connection(|mut db_conn| async move {
+            oauth2_applications::table
+                .find(user_info.oauth2.application_id)
+                .get_result(&mut db_conn)
+                .await
+                .map_err(Error::from)
+        })
         .await?;
 
     let authorisation_code = AuthorisationCode::builder()
