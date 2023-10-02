e028205938b4abd1e92457db3090cc664aafed80
aumetra <aumetra@cryptolab.net>
2023-10-02 17:06:55 +0200

    Remove `diesel_migrations_async` (#362)
    * Remove `diesel_migrations_async`
    
    * move pool construction after blocking task

--- crates/kitsune-db/src/lib.rs
+++ crates/kitsune-db/src/lib.rs
@@ -7,11 +7,13 @@
     forbidden_lint_groups
 )]
 
+use diesel::Connection;
 use diesel_async::{
+    async_connection_wrapper::AsyncConnectionWrapper,
     pooled_connection::{deadpool::Pool, AsyncDieselConnectionManager},
     AsyncPgConnection,
 };
-use diesel_migrations_async::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use tracing_log::LogTracer;
 
 pub use crate::{
@@ -37,6 +39,22 @@ pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 pub async fn connect(conn_str: &str, max_pool_size: usize) -> Result<PgPool> {
     LogTracer::init().ok();
 
+    tokio::task::spawn_blocking({
+        let conn_str = conn_str.to_string();
+
+        move || {
+            let mut migration_conn =
+                AsyncConnectionWrapper::<AsyncPgConnection>::establish(conn_str.as_str())?;
+
+            migration_conn
+                .run_pending_migrations(MIGRATIONS)
+                .map_err(Error::Migration)?;
+
+            Ok::<_, Error>(())
+        }
+    })
+    .await??;
+
     let config = AsyncDieselConnectionManager::<AsyncPgConnection>::new(conn_str);
     let pool = Pool::builder(config)
         .max_size(max_pool_size)
@@ -44,9 +62,6 @@ pub async fn connect(conn_str: &str, max_pool_size: usize) -> Result<PgPool> {
         .unwrap();
 
     let mut conn = pool.get().await?;
-    conn.run_pending_migrations(MIGRATIONS)
-        .await
-        .map_err(Error::Migration)?;
 
     kitsune_language::generate_postgres_enum(&mut conn, "language_iso_code").await?;
     kitsune_language::generate_regconfig_function(
