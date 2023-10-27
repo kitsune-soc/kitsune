fd7602dbed93eb9bdafa88eeb7dcb2527a374f5d
aumetra <aumetra@cryptolab.net>
2023-10-27 22:37:27 +0200

    Add `masto-id-convert` crate to monorepo (#391)
    * Add `masto-id-convert` crate to monorepo
    
    * remove large macro

---
+++ lib/masto-id-convert/benches/process.rs
@@ -0,0 +1,14 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn process_bench(c: &mut Criterion) {
+    c.bench_function("process integer 110368129515784116", |b| {
+        b.iter(|| masto_id_convert::process_u64(black_box(110368129515784116)))
+    });
+
+    c.bench_function("process ASCII 110368129515784116", |b| {
+        b.iter(|| masto_id_convert::process(black_box("110368129515784116")))
+    });
+}
+
+criterion_group!(benches, process_bench);
+criterion_main!(benches);
