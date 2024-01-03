6dbab6e11a8a3de7e9763f11619bbc7fe45cdeb4
aumetra <aumetra@cryptolab.net>
2024-01-03 18:10:00 +0100

    Restructure SCSS compilation

--- xtask/src/build_scss.rs
+++ xtask/src/build_scss.rs
@@ -1,24 +1,9 @@
-use glob::glob;
-use rsass::output::{Format, Style};
-use std::{fs, path::PathBuf};
+use std::path::PathBuf;
 
+#[inline]
 pub fn build_scss(path: PathBuf) -> anyhow::Result<()> {
     info!("Building backend SCSS..");
-
-    let scss_format = Format {
-        style: Style::Compressed,
-        ..Default::default()
-    };
-
-    let pattern = format!("{}/*.scss", path.display());
-    for file in glob(&pattern)? {
-        let mut path = file?;
-        info!("Compiling \"{}\" into CSS", path.display());
-
-        let compiled_css = rsass::compile_scss_path(&path, scss_format)?;
-        path.set_extension("css");
-        fs::write(path, compiled_css)?;
-    }
+    kitsune_scss_compiler::compile(path)?;
 
     Ok(())
 }
