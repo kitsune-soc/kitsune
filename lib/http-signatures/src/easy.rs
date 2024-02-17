cd5d18a7089b41ba9cd16b580ee1fd65117b5655
aumetra <aumetra@cryptolab.net>
2024-02-17 21:44:05 +0100

    add skeleton for the easy api

---
+++ lib/http-signatures/src/easy.rs
@@ -0,0 +1,7 @@
+pub async fn sign<B>(req: http::Request<B>) -> http::Request<B> {
+    todo!();
+}
+
+pub async fn verify<B>(req: &http::Request<B>) -> bool {
+    todo!();
+}
--- lib/http-signatures/src/lib.rs
+++ lib/http-signatures/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod cavage;
 pub mod crypto;
+pub mod easy;
 
 pub const REQUIRED_GET_HEADERS: &[&str] = &["host", "date"];
 pub const REQUIRED_POST_HEADERS: &[&str] = &["host", "date", "content-type", "digest"];
