74ff04fead264e22bacf038b1ef429183695b368
aumetra <aumetra@cryptolab.net>
2024-02-19 12:59:16 +0100

    fix warning

--- lib/http-signatures/src/lib.rs
+++ lib/http-signatures/src/lib.rs
@@ -3,6 +3,6 @@ use http::HeaderName;
 pub mod cavage;
 pub mod crypto;
 
-type BoxError = Box<dyn std::error::Error + Send + Sync>;
+pub type BoxError = Box<dyn std::error::Error + Send + Sync>;
 
-static SIGNATURE_HEADER: HeaderName = HeaderName::from_static("signature");
+pub static SIGNATURE_HEADER: HeaderName = HeaderName::from_static("signature");
