adcadb259f94b091c6433873eb29fb33d489fb03
aumetra <aumetra@cryptolab.net>
2024-02-28 19:28:46 +0100

    convert `activityTypes` to set

--- crates/kitsune-wasm-mrf/mrf-manifest/src/lib.rs
+++ crates/kitsune-wasm-mrf/mrf-manifest/src/lib.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::borrow::Cow;
+use std::ops::{Deref, DerefMut};
 
 #[cfg(feature = "parse")]
 pub use self::parse::ParseError;
@@ -9,7 +9,45 @@ mod parse;
 #[cfg(feature = "serialise")]
 mod serialise;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
+#[serde(transparent)]
+pub struct ActivitySet<'a>(#[serde(borrow)] pub ahash::HashSet<&'a str>);
+
+impl ActivitySet<'_> {
+    /// Does the set of requested activity types contain `*`?
+    pub fn all_activities(&self) -> bool {
+        self.0.contains("*")
+    }
+}
+
+impl<'a> Deref for ActivitySet<'a> {
+    type Target = ahash::HashSet<&'a str>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for ActivitySet<'_> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<'a> From<ActivitySet<'a>> for ahash::HashSet<&'a str> {
+    fn from(value: ActivitySet<'a>) -> Self {
+        value.0
+    }
+}
+
+impl<'a> From<ahash::HashSet<&'a str>> for ActivitySet<'a> {
+    fn from(value: ahash::HashSet<&'a str>) -> Self {
+        Self(value)
+    }
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
 #[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 #[serde(rename_all = "camelCase")]
 #[non_exhaustive]
@@ -18,7 +56,7 @@ pub enum ApiVersion {
 }
 
 /// Manifest of MRF modules
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 #[serde(rename_all = "camelCase", tag = "manifestVersion")]
 #[non_exhaustive]
@@ -27,7 +65,7 @@ pub enum Manifest<'a> {
     V1(ManifestV1<'a>),
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 #[serde(rename_all = "camelCase")]
 pub struct ManifestV1<'a> {
@@ -35,16 +73,13 @@ pub struct ManifestV1<'a> {
     pub api_version: ApiVersion,
 
     /// Name of the MRF module
-    #[serde(borrow)]
-    pub name: Cow<'a, str>,
+    pub name: &'a str,
 
     /// Version of the MRF module
-    #[serde(borrow)]
-    pub version: Cow<'a, semver::Version>,
+    pub version: semver::Version,
 
     /// Activity types passed to the MRF module
     ///
     /// `*` matching all types
-    #[serde(borrow)]
-    pub activity_types: Cow<'a, [Cow<'a, str>]>,
+    pub activity_types: ActivitySet<'a>,
 }
