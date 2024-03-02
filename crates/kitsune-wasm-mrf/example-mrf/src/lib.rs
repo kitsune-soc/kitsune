c64d061e60df5c1ec8bd96066aee4dc27d3666ee
aumetra <aumetra@cryptolab.net>
2024-03-02 18:41:19 +0100

    add kv example

--- crates/kitsune-wasm-mrf/example-mrf/src/lib.rs
+++ crates/kitsune-wasm-mrf/example-mrf/src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(clippy::missing_safety_doc, clippy::transmute_int_to_bool, unsafe_code)]
 
-use self::wasi::logging::logging::{self, Level};
+use self::{
+    fep::mrf::keyvalue::{self, Bucket},
+    wasi::logging::logging::{self, Level},
+};
 
 wit_bindgen::generate!();
 
@@ -17,6 +20,12 @@ impl Guest for Mrf {
             "example-mrf",
             "we got an activity! that's cool!",
         );
+
+        // We even have a key-value store! Check this out:
+        let bucket = Bucket::open_bucket("example-bucket").unwrap();
+        keyvalue::set(&bucket, "hello", b"world").unwrap();
+        assert!(keyvalue::exists(&bucket, "hello").unwrap());
+        keyvalue::delete(&bucket, "hello").unwrap();
 
         Ok(activity)
     }
