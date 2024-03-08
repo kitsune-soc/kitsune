6f60d5372da958afd93a038c2bf4cfc889f4a219
aumetra <aumetra@cryptolab.net>
2024-03-08 17:59:03 +0100

    impl redis backend

--- crates/kitsune-wasm-mrf/src/kv_storage/redis.rs
+++ crates/kitsune-wasm-mrf/src/kv_storage/redis.rs
@@ -1,3 +1,59 @@
-pub struct RedisBackend {}
+use super::BoxError;
+use redis::{aio::ConnectionManager, AsyncCommands};
 
-pub struct RedisBucketBackend {}
+const REDIS_NAMESPACE: &str = "MRF-KV-STORE";
+
+pub struct RedisBackend {
+    module_name: String,
+    pool: multiplex_pool::Pool<ConnectionManager>,
+}
+
+impl super::Backend for RedisBackend {
+    type Bucket = RedisBucketBackend;
+
+    async fn open(&self, name: &str) -> Result<Self::Bucket, BoxError> {
+        Ok(RedisBucketBackend {
+            name: format!("{REDIS_NAMESPACE}:{}:{name}", self.module_name),
+            pool: self.pool.clone(),
+        })
+    }
+}
+
+pub struct RedisBucketBackend {
+    name: String,
+    pool: multiplex_pool::Pool<ConnectionManager>,
+}
+
+impl super::BucketBackend for RedisBucketBackend {
+    async fn exists(&self, key: &str) -> Result<bool, BoxError> {
+        self.pool
+            .get()
+            .hexists(&self.name, key)
+            .await
+            .map_err(Into::into)
+    }
+
+    async fn delete(&self, key: &str) -> Result<(), BoxError> {
+        self.pool
+            .get()
+            .hdel(&self.name, key)
+            .await
+            .map_err(Into::into)
+    }
+
+    async fn get(&self, key: &str) -> Result<Option<Vec<u8>>, BoxError> {
+        self.pool
+            .get()
+            .hget(&self.name, key)
+            .await
+            .map_err(Into::into)
+    }
+
+    async fn set(&self, key: &str, value: &[u8]) -> Result<(), BoxError> {
+        self.pool
+            .get()
+            .hset(&self.name, key, value)
+            .await
+            .map_err(Into::into)
+    }
+}
