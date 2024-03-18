c6ec4b198c3e88687d603d8c7252ced2f2aab272
Aumetra Weisman <aumetra@cryptolab.net>
2024-03-18 22:00:01 +0100

    add graphql tests

---
+++ lib/speedy-uuid/tests/async_graphql.rs
@@ -0,0 +1,34 @@
+#![cfg(feature = "async-graphql")]
+
+use async_graphql::{connection::CursorType, ScalarType};
+use speedy_uuid::Uuid;
+
+const UUID: &str = "38058daf-b2cd-4832-902a-83583ac07e28";
+const UUID_BYTES: [u8; 16] = [
+    0x38, 0x05, 0x8d, 0xaf, 0xb2, 0xcd, 0x48, 0x32, 0x90, 0x2a, 0x83, 0x58, 0x3a, 0xc0, 0x7e, 0x28,
+];
+
+#[test]
+fn cursor_encode_decode() {
+    let parsed_cursor: Uuid = CursorType::decode_cursor(UUID).unwrap();
+    assert_eq!(parsed_cursor.as_bytes(), &UUID_BYTES);
+
+    let result: Result<Uuid, speedy_uuid::Error> = CursorType::decode_cursor("NOT A UUID");
+    assert!(result.is_err());
+
+    let encoded_cursor = CursorType::encode_cursor(&parsed_cursor);
+    assert_eq!(encoded_cursor, UUID);
+}
+
+#[test]
+fn scalar_encode_decode() {
+    let parsed_scalar: Uuid = ScalarType::parse(async_graphql::Value::String(UUID.into())).unwrap();
+    assert_eq!(parsed_scalar.as_bytes(), &UUID_BYTES);
+
+    let result: async_graphql::InputValueResult<Uuid> =
+        ScalarType::parse(async_graphql::Value::Null);
+    assert!(result.is_err());
+
+    let encoded_scalar = ScalarType::to_value(&parsed_scalar);
+    assert_eq!(encoded_scalar, async_graphql::Value::String(UUID.into()));
+}
