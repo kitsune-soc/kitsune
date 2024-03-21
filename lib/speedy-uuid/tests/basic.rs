c4e0b3c1a201db3a563b6eadb7c35d445e39e138
Aumetra Weisman <aumetra@cryptolab.net>
2024-03-22 00:09:19 +0100

    rename test

--- lib/speedy-uuid/tests/basic.rs
+++ lib/speedy-uuid/tests/basic.rs
@@ -4,7 +4,7 @@ use std::str::FromStr;
 const UUID: &str = "38058daf-b2cd-4832-902a-83583ac07e28";
 
 #[test]
-fn parse() {
+fn roundtrip() {
     let uuid = Uuid::from_str(UUID).unwrap();
     assert_eq!(UUID, uuid.to_string());
 }
