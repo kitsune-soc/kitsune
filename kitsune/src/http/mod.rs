dc2101927dbdb423dccf0713ee859cf07f332dad
Aumetra Weisman <aumetra@cryptolab.net>
2024-04-06 18:41:29 +0200

    NGINX-looking fallback fallback, small improvements (#519)
    * small improvements
    
    * make fallback work
    
    * remove potential boxing, move html into own file
    
    * move into separate closure

--- kitsune/src/http/mod.rs
+++ kitsune/src/http/mod.rs
@@ -5,22 +5,36 @@ use self::{
     openapi::api_docs,
 };
 use crate::state::Zustand;
-use axum::{extract::DefaultBodyLimit, Router};
+use axum::{
+    body::HttpBody,
+    extract::DefaultBodyLimit,
+    response::{Html, IntoResponse},
+    Router,
+};
+use bytes::Bytes;
 use color_eyre::eyre::{self, Context};
 use cursiv::CsrfLayer;
+use http::{HeaderName, StatusCode};
+use http_body_util::Either;
 use kitsune_config::server;
-use std::time::Duration;
+use std::{convert::Infallible, time::Duration};
 use tokio::net::TcpListener;
+use tower::{BoxError, Service, ServiceExt};
 use tower_http::{
     catch_panic::CatchPanicLayer,
     cors::CorsLayer,
+    request_id::{MakeRequestUuid, PropagateRequestIdLayer, SetRequestIdLayer},
     services::{ServeDir, ServeFile},
     timeout::TimeoutLayer,
-    trace::TraceLayer,
+    trace::{HttpMakeClassifier, MakeSpan, TraceLayer},
 };
 use tower_stop_using_brave::StopUsingBraveLayer;
 use tower_x_clacks_overhead::XClacksOverheadLayer;
 use utoipa_swagger_ui::SwaggerUi;
+
+const FALLBACK_FALLBACK_INDEX: &str = include_str!("../../templates/fallback-fallback.html");
+
+static X_REQUEST_ID: HeaderName = HeaderName::from_static("x-request-id");
 
 #[cfg(feature = "graphql-api")]
 mod graphql;
@@ -34,10 +48,18 @@ mod util;
 
 pub mod extractor;
 
-pub fn create_router(
-    state: Zustand,
+#[inline]
+fn serve_frontend<B>(
     server_config: &server::Configuration,
-) -> eyre::Result<Router> {
+) -> impl Service<
+    http::Request<B>,
+    Response = http::Response<impl HttpBody<Data = Bytes, Error = BoxError>>,
+    Error = Infallible,
+    Future = impl Send,
+> + Clone
+where
+    B: Send + 'static,
+{
     let frontend_dir = &server_config.frontend_dir;
     let frontend_index_path = {
         let mut tmp = frontend_dir.to_string();
@@ -45,6 +67,41 @@ pub fn create_router(
         tmp
     };
 
+    let handle_response = |response: http::Response<_>| {
+        if response.status() == StatusCode::NOT_FOUND {
+            (StatusCode::NOT_FOUND, Html(FALLBACK_FALLBACK_INDEX))
+                .into_response()
+                .map(Either::Left)
+        } else {
+            response.map(Either::Right)
+        }
+    };
+
+    ServeDir::new(frontend_dir.as_str())
+        .fallback(ServeFile::new(frontend_index_path))
+        .map_future(move |result_fut| async move {
+            let result = result_fut.await;
+            result.map(handle_response)
+        })
+}
+
+#[inline]
+fn trace_layer<B>() -> TraceLayer<HttpMakeClassifier, impl MakeSpan<B> + Clone> {
+    TraceLayer::new_for_http().make_span_with(|request: &http::Request<B>| {
+        debug_span!(
+            "request",
+            method = %request.method(),
+            uri = %request.uri(),
+            version = ?request.version(),
+            request_id = ?request.headers().get(&X_REQUEST_ID).unwrap(),
+        )
+    })
+}
+
+pub fn create_router(
+    state: Zustand,
+    server_config: &server::Configuration,
+) -> eyre::Result<Router> {
     // This warning will come up if the server is compiled without the Mastodon API compatibility
     #[allow(unused_mut)]
     let mut router = Router::new()
@@ -78,9 +135,7 @@ pub fn create_router(
 
     router = router
         .merge(SwaggerUi::new("/swagger-ui").url("/api-doc/openapi.json", api_docs()))
-        .fallback_service(
-            ServeDir::new(frontend_dir.as_str()).fallback(ServeFile::new(frontend_index_path)),
-        );
+        .fallback_service(serve_frontend(server_config));
 
     if !server_config.clacks_overhead.is_empty() {
         let clacks_overhead_layer =
@@ -98,11 +153,18 @@ pub fn create_router(
         .layer(CatchPanicLayer::new())
         .layer(CorsLayer::permissive())
         .layer(CsrfLayer::generate()) // TODO: Make this configurable instead of random
-        .layer(DefaultBodyLimit::max(server_config.max_upload_size))
+        .layer(DefaultBodyLimit::max(
+            server_config.max_upload_size.to_bytes() as usize,
+        ))
         .layer(TimeoutLayer::new(Duration::from_secs(
             server_config.request_timeout_secs,
         )))
-        .layer(TraceLayer::new_for_http())
+        .layer(trace_layer())
+        .layer(PropagateRequestIdLayer::new(X_REQUEST_ID.clone()))
+        .layer(SetRequestIdLayer::new(
+            X_REQUEST_ID.clone(),
+            MakeRequestUuid,
+        ))
         .with_state(state))
 }
 
