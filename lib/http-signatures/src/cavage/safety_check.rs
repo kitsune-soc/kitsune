a4bb376bdc7e46277cc7c7542dcbebd7a3f3eb51
Aumetra Weisman <aumetra@cryptolab.net>
2024-04-22 23:56:21 +0200

    remove slow test

--- lib/http-signatures/src/cavage/safety_check.rs
+++ lib/http-signatures/src/cavage/safety_check.rs
@@ -116,23 +116,3 @@ where
 
     Ok(())
 }
-
-#[cfg(test)]
-mod test {
-    use super::is_subset;
-    use proptest::{prop_assert_eq, proptest};
-    use std::collections::HashSet;
-
-    proptest! {
-        #[test]
-        fn subset_behaves_equal(left: HashSet<String>, right: HashSet<String>) {
-            let vec_left = left.iter().collect::<Vec<_>>();
-            let vec_right = right.iter().collect::<Vec<_>>();
-
-            let slice_subset = is_subset(&vec_left, &vec_right);
-            let set_subset = left.is_subset(&right);
-
-            prop_assert_eq!(slice_subset, set_subset);
-        }
-    }
-}
