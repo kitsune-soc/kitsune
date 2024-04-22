a1df2f53dfbcfd2343ffbe1e96cf8f091a19dfdc
Aumetra Weisman <aumetra@cryptolab.net>
2024-04-22 23:54:43 +0200

    changes

--- crates/kitsune-oidc/src/http.rs
+++ crates/kitsune-oidc/src/http.rs
@@ -1,16 +1,20 @@
+use http_body_util::BodyExt;
 use kitsune_http_client::Client as HttpClient;
 use once_cell::sync::Lazy;
 use openidconnect::{HttpRequest, HttpResponse};
 
 static HTTP_CLIENT: Lazy<HttpClient> = Lazy::new(HttpClient::default);
 
+#[inline]
 pub async fn async_client(req: HttpRequest) -> Result<HttpResponse, kitsune_http_client::Error> {
     let response = HTTP_CLIENT.execute(req.map(Into::into)).await?;
 
-    let mut builder = http::Response::builder()
-        .status(response.status())
-        .version(response.version());
-    *builder.headers_mut().unwrap() = response.headers().clone();
+    let (parts, body) = response.into_inner().into_parts();
+    let body = body
+        .collect()
+        .await
+        .map_err(kitsune_http_client::Error::new)?
+        .to_bytes();
 
-    Ok(builder.body(response.bytes().await?.to_vec()).unwrap())
+    Ok(HttpResponse::from_parts(parts, body.to_vec()))
 }
