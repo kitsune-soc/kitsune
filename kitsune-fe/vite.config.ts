195cf61d90c54103f92d808428e698ffef6098c6
Aumetra Weisman <aumetra@cryptolab.net>
2024-05-01 17:48:53 +0200

    add graphql client

--- kitsune-fe/vite.config.ts
+++ kitsune-fe/vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import houdini from 'houdini/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [houdini(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
