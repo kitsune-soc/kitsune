5281b491257237747ae992ab7e8b350787eb5fa6
Aumetra Weisman <aumetra@cryptolab.net>
2024-05-01 18:19:47 +0200

    add unplugin-icons

--- kitsune-fe/vite.config.ts
+++ kitsune-fe/vite.config.ts
@@ -1,9 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import houdini from 'houdini/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [houdini(), sveltekit()],
+	plugins: [
+		houdini(),
+		sveltekit(),
+		Icons({
+			autoInstall: true,
+			compiler: 'svelte'
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
