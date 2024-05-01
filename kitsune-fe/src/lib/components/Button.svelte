611cacb533cf59b2913a1a347d8d040c0345bcfc
Aumetra Weisman <aumetra@cryptolab.net>
2024-05-01 15:56:00 +0200

    rewrite frontpage

---
+++ kitsune-fe/src/lib/components/Button.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+
+	let {
+		children,
+		...rest
+	}:
+		| {
+				children: Snippet;
+		  }
+		| any = $props();
+</script>
+
+<button {...rest}>
+	{@render children()}
+</button>
+
+<style lang="scss">
+	@use '../../styles/colours' as *;
+
+	button {
+		transition: 0.5s;
+		cursor: pointer;
+
+		border: 0;
+		border-radius: 5px;
+
+		background-color: $shade1dark;
+
+		padding: 10px;
+		width: 100px;
+		font-size: 16px;
+
+		&:hover {
+			background-color: $shade2dark;
+		}
+	}
+</style>
