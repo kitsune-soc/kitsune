22eda853118cd5f84bd97fe272a48bc1b0cd4b4c
aumetra <aumetra@cryptolab.net>
2024-05-05 01:38:10 +0200

    wire up cache, add docs

--- crates/kitsune-wasm-mrf/src/cache.rs
+++ crates/kitsune-wasm-mrf/src/cache.rs
@@ -18,11 +18,14 @@ impl Cache {
     }
 
     #[inline]
+    #[instrument(skip_all)]
     pub fn load(&self, engine: &wasmtime::Engine, component: &[u8]) -> Result<Option<Component>> {
         let hash = blake3::hash(component);
         let Some(precompiled) = self.inner.get(hash.as_bytes())? else {
             return Ok(None);
         };
+
+        debug!(hash = %hash.to_hex(), "hit component cache");
 
         // SAFETY: The function is defined as unsafe since it is only doing very simple checks whether the precompiled component inside is actually valid
         //         But since we source our cache from disk, we can assume that the files are fine. If they aren't, the user has tempered with them or they were otherwise corrupted.
@@ -32,12 +35,15 @@ impl Cache {
     }
 
     #[inline]
+    #[instrument(skip_all)]
     pub fn store(&self, source: &[u8], component: &Component) -> Result<()> {
         let hash = blake3::hash(source);
         self.inner.insert(
             hash.as_bytes(),
             component.serialize().map_err(eyre::Report::msg)?,
         )?;
+
+        debug!(hash = %hash.to_hex(), "stored component in cache");
 
         Ok(())
     }
