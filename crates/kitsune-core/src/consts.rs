02381606da04f001e9b3d0ff4d5aaed1813d50b8
Aumetra Weisman <aumetra@cryptolab.net>
2024-07-20 21:05:00 +0200

    Cargo update

--- crates/kitsune-core/src/consts.rs
+++ crates/kitsune-core/src/consts.rs
@@ -3,5 +3,6 @@ use const_format::concatcp;
 pub const API_MAX_LIMIT: usize = 40;
 pub const MAX_EMOJI_SHORTCODE_LENGTH: usize = 64;
 pub const MAX_MEDIA_DESCRIPTION_LENGTH: usize = 5000;
-pub const USER_AGENT: &str = concatcp!(env!("CARGO_PRIMARY_PACKAGE"), "/", VERSION);
+pub const PROJECT_IDENTIFIER: &str = env!("CARGO_PRIMARY_PACKAGE");
+pub const USER_AGENT: &str = concatcp!(PROJECT_IDENTIFIER, "/", VERSION);
 pub const VERSION: &str = concatcp!(env!("CARGO_PKG_VERSION"), "-", env!("VERGEN_GIT_SHA"));
