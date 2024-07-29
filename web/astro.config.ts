88c7bffedca07c3a815ec44df38b9c3929d8cfb2
Aumetra Weisman <aumetra@cryptolab.net>
2024-07-29 20:10:04 +0200

    Set CNAME

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://joinkitsune.org",
   integrations: [
     starlight({
       title: "Kitsune",
