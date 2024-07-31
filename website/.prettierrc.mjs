a7e1a92de5fde52d14b88ac59426dd03068d353c
Aumetra Weisman <aumetra@cryptolab.net>
2024-07-31 22:14:44 +0200

    Tweak colour scheme (#566)

--- website/.prettierrc.mjs
+++ website/.prettierrc.mjs
@@ -1,12 +1,14 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ['prettier-plugin-astro'],
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
+        parser: 'astro',
       },
     },
   ],
+
+  singleQuote: true,
 };
