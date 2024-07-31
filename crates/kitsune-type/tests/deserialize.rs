0a34984865c9153f69170e8079cc581ff38c9183
Aumetra Weisman <aumetra@cryptolab.net>
2024-07-31 12:52:05 +0200

    use rstest to generate tests from fixtures

--- crates/kitsune-type/tests/deserialize.rs
+++ crates/kitsune-type/tests/deserialize.rs
@@ -1,31 +1,15 @@
-use core::panic;
+use rstest::rstest;
+use std::{fs, path::PathBuf};
 
-use rust_embed::Embed;
-
-#[derive(Embed)]
-#[folder = "tests/actors"]
-struct Actors;
-
-#[derive(Embed)]
-#[folder = "tests/objects"]
-struct Objects;
-
-#[test]
-fn actors() {
-    for actor_path in Actors::iter() {
-        let file = Actors::get(&actor_path).unwrap();
-        if let Err(error) = sonic_rs::from_slice::<kitsune_type::ap::actor::Actor>(&file.data) {
-            panic!("Failed to deserialize actor at path {actor_path}: {error}");
-        }
-    }
+#[rstest]
+fn actors(#[files("tests/actors/*")] path: PathBuf) {
+    let data = fs::read(path).unwrap();
+    sonic_rs::from_slice::<kitsune_type::ap::actor::Actor>(&data)
+        .expect("Failed to deserialize actor");
 }
 
-#[test]
-fn objects() {
-    for object_path in Objects::iter() {
-        let file = Objects::get(&object_path).unwrap();
-        if let Err(error) = sonic_rs::from_slice::<kitsune_type::ap::Object>(&file.data) {
-            panic!("Failed to deserialize object at path {object_path}: {error}");
-        }
-    }
+#[rstest]
+fn objects(#[files("tests/objects/*")] path: PathBuf) {
+    let data = fs::read(path).unwrap();
+    sonic_rs::from_slice::<kitsune_type::ap::Object>(&data).expect("Failed to deserialize object");
 }
