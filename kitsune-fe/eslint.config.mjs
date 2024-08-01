a4d53e54fea1f5669ea6716bac02f5340167a43a
Aumetra Weisman <aumetra@cryptolab.net>
2024-08-01 12:53:25 +0200

    migrate eslint

---
+++ kitsune-fe/eslint.config.mjs
@@ -0,0 +1,75 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import parser from 'svelte-eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+});
+
+/** @type { import("eslint").Linter.Config } */
+export default [
+	{
+		ignores: [
+			'**/.DS_Store',
+			'**/node_modules',
+			'build',
+			'.svelte-kit',
+			'$houdini',
+			'package',
+			'**/.env',
+			'**/.env.*',
+			'!**/.env.example',
+			'**/pnpm-lock.yaml',
+			'**/package-lock.json',
+			'**/yarn.lock'
+		]
+	},
+	...compat.extends(
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	),
+	{
+		plugins: {
+			'@typescript-eslint': typescriptEslint
+		},
+
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			},
+
+			parser: tsParser,
+			ecmaVersion: 2020,
+			sourceType: 'module',
+
+			parserOptions: {
+				extraFileExtensions: ['.svelte']
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+
+		languageOptions: {
+			parser: parser,
+			ecmaVersion: 5,
+			sourceType: 'script',
+
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	}
+];
