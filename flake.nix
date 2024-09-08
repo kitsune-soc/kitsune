46ab774a581f99dbbf3edd778bb4cedfde3abf9c
Aumetra Weisman <aumetra@cryptolab.net>
2024-09-08 13:42:20 +0200

    fix deps hash

--- flake.nix
+++ flake.nix
@@ -139,7 +139,7 @@
                 inherit version;
 
                 src = "${src}/kitsune-fe";
-                npmDepsHash = "sha256-jC9zB68BKGEEJhPCprvmQDl5WV0bzm9ZaIfO+K9O+wc=";
+                npmDepsHash = "sha256-zF36ES28Ok6hDYw59ZfvoO2hyeQhEnfNqzYMo8X9iGw=";
 
                 npmFlags = [ "--legacy-peer-deps" ];
 
