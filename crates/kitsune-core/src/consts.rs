a34c200c14a4299e2f772cbaaade1676603e9b56
Aumetra Weisman <aumetra@cryptolab.net>
2024-09-08 14:44:26 +0200

    fix compilation for libraries

--- crates/kitsune-core/src/consts.rs
+++ crates/kitsune-core/src/consts.rs
@@ -4,6 +4,16 @@ use git_version::git_version;
 pub const API_MAX_LIMIT: usize = 40;
 pub const MAX_EMOJI_SHORTCODE_LENGTH: usize = 64;
 pub const MAX_MEDIA_DESCRIPTION_LENGTH: usize = 5000;
-pub const PROJECT_IDENTIFIER: &str = env!("CARGO_PRIMARY_PACKAGE");
+
+pub const PROJECT_IDENTIFIER: &str = const {
+    const FALLBACK_NAME: &str = "kitsune";
+
+    if let Some(package_name) = option_env!("CARGO_PRIMARY_PACKAGE") {
+        package_name
+    } else {
+        FALLBACK_NAME
+    }
+};
+
 pub const USER_AGENT: &str = concatcp!(PROJECT_IDENTIFIER, "/", VERSION);
 pub const VERSION: &str = concatcp!(env!("CARGO_PKG_VERSION"), "-", git_version!());
