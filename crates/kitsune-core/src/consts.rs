8fece6cb29f250fbcdb2fc8ffe275ec2c9c57332
Aumetra Weisman <aumetra@cryptolab.net>
2024-09-08 12:54:10 +0000

    Replace `vergen` (#580)

--- crates/kitsune-core/src/consts.rs
+++ crates/kitsune-core/src/consts.rs
@@ -1,8 +1,23 @@
 use const_format::concatcp;
+use git_version::git_version;
 
 pub const API_MAX_LIMIT: usize = 40;
 pub const MAX_EMOJI_SHORTCODE_LENGTH: usize = 64;
 pub const MAX_MEDIA_DESCRIPTION_LENGTH: usize = 5000;
-pub const PROJECT_IDENTIFIER: &str = env!("CARGO_PRIMARY_PACKAGE");
+
+pub const PROJECT_IDENTIFIER: &str = const {
+    const FALLBACK_NAME: &str = "kitsune";
+
+    if let Some(package_name) = option_env!("CARGO_PRIMARY_PACKAGE") {
+        package_name
+    } else {
+        FALLBACK_NAME
+    }
+};
+
 pub const USER_AGENT: &str = concatcp!(PROJECT_IDENTIFIER, "/", VERSION);
-pub const VERSION: &str = concatcp!(env!("CARGO_PKG_VERSION"), "-", env!("VERGEN_GIT_SHA"));
+pub const VERSION: &str = concatcp!(
+    env!("CARGO_PKG_VERSION"),
+    "-",
+    git_version!(fallback = "unknown")
+);
