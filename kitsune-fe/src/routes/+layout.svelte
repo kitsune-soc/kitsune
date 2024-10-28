d433d5a8a65e3fae7c562a0542ca7374c336a0d5
Aumetra Weisman <aumetra@cryptolab.net>
2024-10-28 01:55:20 +0100

    fix tablet footer

--- kitsune-fe/src/routes/+layout.svelte
+++ kitsune-fe/src/routes/+layout.svelte
@@ -20,7 +20,7 @@
 
 {@render children()}
 
-<footer class="text-sm max-lg:mb-5 max-lg:text-center lg:fixed lg:bottom-3 lg:left-3">
+<footer class="w-full text-sm max-lg:mb-5 max-lg:text-center lg:fixed lg:bottom-3 lg:left-3">
 	<p>
 		Backend version: {backendVersion}
 		<br />Frontend version: {frontendVersion}
