2a3d7ebc8cee6692f5bbd394bb3c77bd28abd682
Aumetra Weisman <aumetra@cryptolab.net>
2024-11-13 00:50:32 +0100

    get rid of future wrapper

--- crates/kitsune-http-client/src/resolver.rs
+++ crates/kitsune-http-client/src/resolver.rs
@@ -1,11 +1,8 @@
 use futures_util::{future::BoxFuture, FutureExt};
 use hickory_resolver::{lookup_ip::LookupIpIntoIter, TokioResolver};
 use hyper_util::client::legacy::connect::dns::Name;
-use pin_project::pin_project;
 use std::{
-    future::Future,
     net::SocketAddr,
-    pin::Pin,
     task::{self, Poll},
 };
 use tower::{BoxError, Service};
@@ -24,21 +21,6 @@ impl Iterator for ResolveIter {
     }
 }
 
-#[pin_project]
-pub struct ResolveFut {
-    #[pin]
-    inner: BoxFuture<'static, Result<ResolveIter, BoxError>>,
-}
-
-impl Future for ResolveFut {
-    type Output = Result<ResolveIter, BoxError>;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Self::Output> {
-        let this = self.project();
-        this.inner.poll(cx)
-    }
-}
-
 #[derive(Clone)]
 pub struct Resolver {
     inner: TokioResolver,
@@ -53,7 +35,7 @@ impl From<TokioResolver> for Resolver {
 impl Service<Name> for Resolver {
     type Error = BoxError;
     type Response = ResolveIter;
-    type Future = ResolveFut;
+    type Future = BoxFuture<'static, Result<ResolveIter, BoxError>>;
 
     fn poll_ready(&mut self, _cx: &mut task::Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
@@ -61,14 +43,13 @@ impl Service<Name> for Resolver {
 
     fn call(&mut self, req: Name) -> Self::Future {
         let dns_client = self.inner.clone();
-        let inner = async move {
+
+        async move {
             let lookup_ips = dns_client.lookup_ip(req.as_str()).await?;
             Ok(ResolveIter {
                 inner: lookup_ips.into_iter(),
             })
         }
-        .boxed();
-
-        ResolveFut { inner }
+        .boxed()
     }
 }
