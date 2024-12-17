f8ea1a6b4c403ab0ea4d62670c6770796f14e8d1
Aumetra Weisman <aumetra@cryptolab.net>
2024-12-17 12:35:21 +0100

    add basic auth bench

---
+++ lib/komainu/benches/basic_auth.rs
@@ -0,0 +1,21 @@
+use divan::{black_box, black_box_drop};
+use komainu::extractor::BasicAuth;
+
+#[divan::bench]
+fn rfc_value(b: divan::Bencher<'_, '_>) {
+    let mut map = http::HeaderMap::new();
+    map.insert(
+        http::header::AUTHORIZATION,
+        http::HeaderValue::from_static("Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="),
+    );
+
+    b.bench(|| {
+        let auth = BasicAuth::extract(black_box(&map)).unwrap();
+        black_box_drop((auth.username(), auth.password()));
+        auth
+    });
+}
+
+fn main() {
+    divan::main();
+}
