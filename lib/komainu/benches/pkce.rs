980de63cd6d15fdc780d11ef3b2fb09d53c3a5b2
Aumetra Weisman <aumetra@cryptolab.net>
2024-12-17 12:25:50 +0100

    pkce bench

---
+++ lib/komainu/benches/pkce.rs
@@ -0,0 +1,32 @@
+use divan::black_box;
+use komainu::flow::{PkceMethod, PkcePayload};
+use std::borrow::Cow;
+
+#[divan::bench]
+fn s256() -> komainu::Result<()> {
+    let verifier_base64 = "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk";
+    let challenge_base64 = "E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM";
+
+    let payload = PkcePayload {
+        method: black_box(PkceMethod::S256),
+        challenge: black_box(Cow::Borrowed(challenge_base64)),
+    };
+
+    payload.verify(black_box(verifier_base64))
+}
+
+#[divan::bench]
+fn none() -> komainu::Result<()> {
+    let value = "arbitrary value";
+
+    let payload = PkcePayload {
+        method: black_box(PkceMethod::None),
+        challenge: black_box(Cow::Borrowed(value)),
+    };
+
+    payload.verify(black_box(value))
+}
+
+fn main() {
+    divan::main();
+}
