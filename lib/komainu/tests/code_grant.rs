403776f10bf96f95dd7d6d6acc7c164a22c986ef
Aumetra Weisman <aumetra@cryptolab.net>
2025-01-16 00:13:08 +0100

    more codegrant tests

--- lib/komainu/tests/code_grant.rs
+++ lib/komainu/tests/code_grant.rs
@@ -11,7 +11,7 @@ mod util;
 const RNG_SEED: u64 = 0xBADD1E;
 
 #[futures_test::test]
-async fn success() {
+async fn success_basic() {
     fastrand::seed(RNG_SEED);
     let fixture = Fixture::generate();
 
@@ -19,6 +19,41 @@ async fn success() {
         .scheme("http")
         .authority("komainu.example")
         .path_and_query("/oauth/authorize?response_type=code&client_id=client_1")
+        .build()
+        .unwrap();
+
+    let req = http::Request::builder()
+        .uri(uri)
+        .body(Empty::<Bytes>::new())
+        .unwrap();
+    let req = komainu::Request::read_from(req).await.unwrap();
+
+    let acceptor = {
+        let handle = fixture.code_grant.extract_raw(&req).await.unwrap();
+        handle
+            .accept("user id".into(), &Scope::from_str("read").unwrap())
+            .await
+            .unwrap()
+    };
+
+    let deny = {
+        let handle = fixture.code_grant.extract_raw(&req).await.unwrap();
+        handle.deny()
+    };
+
+    insta::assert_json_snapshot!(SerdeResponse::from(acceptor.into_response()));
+    insta::assert_json_snapshot!(SerdeResponse::from(deny));
+}
+
+#[futures_test::test]
+async fn success_with_state() {
+    fastrand::seed(RNG_SEED);
+    let fixture = Fixture::generate();
+
+    let uri = http::Uri::builder()
+        .scheme("http")
+        .authority("komainu.example")
+        .path_and_query("/oauth/authorize?response_type=code&client_id=client_1&state=cat%20tax")
         .build()
         .unwrap();
 
@@ -54,6 +89,89 @@ async fn redirect_uri_mismatch() {
         .scheme("http")
         .authority("komainu.example")
         .path_and_query("/oauth/authorize?response_type=code&client_id=client_1&redirect_uri=http%3A%2F%2Fbad-redirect-uri.example%2Fhehe")
+        .build()
+        .unwrap();
+
+    let req = http::Request::builder()
+        .uri(uri)
+        .body(Empty::<Bytes>::new())
+        .unwrap();
+    let req = komainu::Request::read_from(req).await.unwrap();
+
+    let err = match fixture.code_grant.extract_raw(&req).await {
+        Ok(..) => panic!(),
+        Err(err) => err,
+    };
+
+    assert!(matches!(err, GrantError::AccessDenied));
+}
+
+#[futures_test::test]
+async fn invalid_response_type() {
+    fastrand::seed(RNG_SEED);
+    let fixture = Fixture::generate();
+
+    let uri = http::Uri::builder()
+        .scheme("http")
+        .authority("komainu.example")
+        .path_and_query("/oauth/authorize?response_type=meoww&client_id=client_1")
+        .build()
+        .unwrap();
+
+    let req = http::Request::builder()
+        .uri(uri)
+        .body(Empty::<Bytes>::new())
+        .unwrap();
+    let req = komainu::Request::read_from(req).await.unwrap();
+
+    let err = match fixture.code_grant.extract_raw(&req).await {
+        Ok(..) => panic!(),
+        Err(err) => err,
+    };
+
+    assert!(matches!(err, GrantError::AccessDenied));
+}
+
+#[futures_test::test]
+async fn success_matching_scopes() {
+    fastrand::seed(RNG_SEED);
+    let fixture = Fixture::generate();
+
+    let uri = http::Uri::builder()
+        .scheme("http")
+        .authority("komainu.example")
+        .path_and_query("/oauth/authorize?response_type=code&client_id=client_1&scope=read%20write")
+        .build()
+        .unwrap();
+
+    let req = http::Request::builder()
+        .uri(uri)
+        .body(Empty::<Bytes>::new())
+        .unwrap();
+    let req = komainu::Request::read_from(req).await.unwrap();
+
+    let acceptor = {
+        let handle = fixture.code_grant.extract_raw(&req).await.unwrap();
+        handle
+            .accept("user id".into(), &Scope::from_str("read").unwrap())
+            .await
+            .unwrap()
+    };
+
+    insta::assert_json_snapshot!(SerdeResponse::from(acceptor.into_response()));
+}
+
+#[futures_test::test]
+async fn too_many_scopes() {
+    fastrand::seed(RNG_SEED);
+    let fixture = Fixture::generate();
+
+    let uri = http::Uri::builder()
+        .scheme("http")
+        .authority("komainu.example")
+        .path_and_query(
+            "/oauth/authorize?response_type=meoww&client_id=client_1&scope=read%20write%20follow",
+        )
         .build()
         .unwrap();
 
