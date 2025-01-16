915853dea31bd4da93529492286c5b6f12dab84a
Aumetra Weisman <aumetra@cryptolab.net>
2025-01-16 22:14:19 +0100

    form story

---
+++ kitsune-fe/src/stories/RegisterForm.stories.svelte
@@ -0,0 +1,16 @@
+<script lang="ts" module>
+	import RegisterFormComponent from '$lib/components/RegisterForm.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { fn } from '@storybook/test';
+
+	const { Story } = defineMeta({
+		title: 'RegisterForm',
+		component: RegisterFormComponent,
+		tags: ['autodocs'],
+		args: {
+			onregister: fn()
+		}
+	});
+</script>
+
+<Story name="Default" />
