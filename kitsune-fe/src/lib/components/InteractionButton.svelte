220e7ed075068b954692f9acf9d5833688b2ab7a
Aumetra Weisman <aumetra@cryptolab.net>
2025-01-18 00:26:35 +0100

    add post component

---
+++ kitsune-fe/src/lib/components/InteractionButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	let { icon, count, oninteract }: { icon: string; count?: number; oninteract?: () => void } =
+		$props();
+</script>
+
+<button
+	class="flex flex-row gap-1.5"
+	onclick={() => {
+		if (oninteract) oninteract();
+	}}
+>
+	<Icon {icon} class="h-auto w-5" />
+
+	{#if count && count > 0}
+		{count}
+	{/if}
+</button>
