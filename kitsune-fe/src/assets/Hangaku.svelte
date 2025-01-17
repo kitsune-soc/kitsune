9358e8dde85b4715faee31096f3cb71f5190bc63
Aumetra Weisman <aumetra@cryptolab.net>
2025-01-17 21:08:10 +0100

    add error page and glitch component

--- kitsune-fe/src/assets/Hangaku.svelte
+++ kitsune-fe/src/assets/Hangaku.svelte
@@ -1,24 +1,13 @@
 <script lang="ts">
-	import { PowerGlitch } from 'powerglitch';
+	import Glitch from '$lib/components/Glitch.svelte';
+
 	import type { HTMLAttributes } from 'svelte/elements';
 
 	import hangakuRaw from './hangaku.svg';
 
 	let props: HTMLAttributes<HTMLDivElement> = $props();
-	let hangakuElement: HTMLImageElement | undefined = $state();
-
-	$effect(() => {
-		PowerGlitch.glitch(hangakuElement, {
-			playMode: 'hover'
-		});
-	});
 </script>
 
-<div {...props}>
-	<img
-		class="m-0 h-auto w-full"
-		bind:this={hangakuElement}
-		src={hangakuRaw}
-		alt="Hangaku (Kitsune mascot)"
-	/>
-</div>
+<Glitch {...props}>
+	<img class="m-0 h-auto w-full" src={hangakuRaw} alt="Hangaku (Kitsune mascot)" />
+</Glitch>
