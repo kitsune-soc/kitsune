cf6dcfe0afc90f23acef62860bb8bfe0cb644997
Aumetra Weisman <aumetra@cryptolab.net>
2025-02-04 17:09:06 +0100

    fix benches

--- lib/speedy-uuid/benches/parse.rs
+++ lib/speedy-uuid/benches/parse.rs
@@ -1,15 +1,16 @@
+use divan::black_box;
 use std::str::FromStr;
 
 const UUID: &str = "0050ee5f-df51-4378-bf68-3ab149d7964e";
 
 #[divan::bench]
 fn normal_uuid() -> Result<uuid::Uuid, uuid::Error> {
-    uuid::Uuid::from_str(UUID)
+    uuid::Uuid::from_str(black_box(UUID))
 }
 
 #[divan::bench]
 fn simd_uuid() -> Result<speedy_uuid::Uuid, speedy_uuid::Error> {
-    speedy_uuid::Uuid::from_str(UUID)
+    speedy_uuid::Uuid::from_str(black_box(UUID))
 }
 
 fn main() {
