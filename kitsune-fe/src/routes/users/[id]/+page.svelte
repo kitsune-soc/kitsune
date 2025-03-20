882548f4ef12ea53435cba36b8439b716bf25986
Aumetra Weisman <aumetra@cryptolab.net>
2025-03-20 19:23:12 +0100

    fix profile page

--- kitsune-fe/src/routes/users/[id]/+page.svelte
+++ kitsune-fe/src/routes/users/[id]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import defaultAvatar from '$assets/default-avatar.png';
 	import defaultHeader from '$assets/default-header.png';
+	import Timeline from '$lib/components/Timeline.svelte';
 	import Zoom from '$lib/components/Zoom.svelte';
+	import type { Post } from '$lib/types/Post';
 
 	import type { PageData } from './$houdini';
 	import IconErrorOutline from '~icons/mdi/error-outline';
@@ -24,6 +26,62 @@
 		headerUrl: $loadAccount.data?.getAccountById?.header?.url ?? defaultHeader,
 		avatarUrl: $loadAccount.data?.getAccountById?.avatar?.url ?? defaultAvatar
 	});
+
+	let posts: Post[] = $derived(
+		$loadAccount.data?.getAccountById?.posts.edges
+			.map((edge) => edge.node)
+			.map((post) => {
+				return {
+					id: post.id,
+					user: {
+						id: post.account.id,
+						name: post.account.displayName ?? post.account.username,
+						username: post.account.username
+					},
+					content: post.content,
+					attachments: post.attachments,
+					replyCount: 0,
+					likeCount: 0,
+					repostCount: 0,
+					url: post.url,
+					createdAt: post.createdAt,
+					visibility: post.visibility
+				};
+			}) ?? []
+	);
+	let lastPostLength = $state(0);
+
+	let reachedEnd = $state(false);
+
+	let timelineMeta: { loadingNewPosts: boolean } = $state({
+		loadingNewPosts: false
+	});
+
+	async function loadTimeline() {
+		console.log(`last post length before: ${lastPostLength}`);
+
+		const result = await loadAccount.loadNextPage();
+		reachedEnd = lastPostLength === result.data?.getAccountById?.posts.edges.length;
+		lastPostLength = result.data?.getAccountById?.posts.edges.length ?? lastPostLength;
+
+		console.log(`reached end: ${reachedEnd}`);
+		console.log(`last post length after: ${lastPostLength}`);
+	}
+
+	async function onendreached() {
+		if (reachedEnd) return;
+		if (timelineMeta.loadingNewPosts) return;
+
+		timelineMeta.loadingNewPosts = true;
+
+		try {
+			await loadTimeline();
+		} catch (error) {
+			console.error(`failed to load posts: ${error}`);
+		}
+
+		timelineMeta.loadingNewPosts = false;
+	}
 </script>
 
 <main class="m-auto w-full max-w-prose">
@@ -64,6 +122,9 @@
 				</div>
 			</div>
 		</div>
-		<p>And here we display all the posts. Yep.</p>
+
+		<div class="mt-5">
+			<Timeline {posts} {onendreached} />
+		</div>
 	{/if}
 </main>
