ed31ea747344154bd4a4b3d7efe85a2a7c66b5cb
aumetra <aumetra@cryptolab.net>
2025-04-15 02:11:23 +0200

    swap out toast impl

--- kitsune-fe/src/lib/components/toast/index.ts
+++ kitsune-fe/src/lib/components/toast/index.ts
@@ -1,24 +1,3 @@
-import { writable } from 'svelte/store';
+import ToastProvider, { pushToast } from './ToastProvider.svelte';
 
-import ToastProvider from './ToastProvider.svelte';
-
-type ToastData = {
-	severity: 'info' | 'error' | 'success';
-	message: string;
-};
-
-const toastStore = writable<ToastData[]>([]);
-
-function pushToast(toast: ToastData) {
-	toastStore.update((value) => value.concat(toast));
-	setTimeout(
-		() =>
-			toastStore.update((value) => {
-				value.shift();
-				return value;
-			}),
-		2_500
-	);
-}
-
-export { ToastProvider, pushToast, toastStore };
+export { ToastProvider, pushToast };
