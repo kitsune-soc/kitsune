185e67478be943500241f97d5e76d0937c0340ba
aumetra <aumetra@cryptolab.net>
2025-04-25 20:15:03 +0200

    delete rust code

--- crates/kitsune-config/src/search.rs
+++ crates/kitsune-config/src/search.rs
@@ -1,17 +1,8 @@
 use serde::{Deserialize, Serialize};
-use smol_str::SmolStr;
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(rename_all = "kebab-case")]
-pub struct MeilisearchConfiguration {
-    pub instance_url: SmolStr,
-    pub api_key: SmolStr,
-}
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case", tag = "type")]
 pub enum Configuration {
-    Meilisearch(MeilisearchConfiguration),
     Sql,
     None,
 }
